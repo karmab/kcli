b726dfdf9884236ab28dac05f4107b2c58326aad
karimb <karimboumedhel@gmail.com>
2016-09-22 00:51:37 +0200

    satellite fix

--- uci/foreman/downstream.sh
+++ uci/foreman/downstream.sh
@@ -1,1 +1,2 @@
-foreman-installer --scenario satellite --foreman-admin-username admin  --foreman-admin-password $PASSWORD --foreman-initial-location $LOCATION --foreman-initial-organization $ORG --capsule-puppet true --foreman-proxy-puppetca true --foreman-proxy-tftp true --enable-foreman-plugin-discovery
+yum -y install satellite-installer wget
+satellite-installer --scenario satellite --foreman-admin-username admin  --foreman-admin-password $PASSWORD --foreman-initial-location $LOCATION --foreman-initial-organization $ORG --capsule-puppet true --foreman-proxy-puppetca true --foreman-proxy-tftp true --enable-foreman-plugin-discovery
--- uci/foreman/install.sh
+++ uci/foreman/install.sh
@@ -2,4 +2,3 @@ export ORG="Karmalabs"
 export LOCATION="Madrid"
 export PASSWORD="unix1234"
 echo `hostname -I` `hostname -s`.uci `hostname -s` >> /etc/hosts
-yum -y install foreman-installer wget
