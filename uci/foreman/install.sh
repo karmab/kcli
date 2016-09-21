54e814c931665f5a068e2f5bc2afd1c0df2b3a0d
karimb <karimboumedhel@gmail.com>
2016-09-22 01:06:19 +0200

    satellite fix

--- uci/foreman/install.sh
+++ uci/foreman/install.sh
@@ -1,4 +1,4 @@
 export ORG="Karmalabs"
 export LOCATION="Madrid"
 export PASSWORD="unix1234"
-echo `hostname -I` `hostname -s`.uci `hostname -s` >> /etc/hosts
+echo `hostname -I` `hostname -s`.cinet `hostname -s` >> /etc/hosts
