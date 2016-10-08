613054217fcb5e71084df9be7b97cd87bc7f87d7
karimb <karimboumedhel@gmail.com>
2016-10-08 19:47:44 +0200

    1.29 released. thanks efenex for your suggestion/contribution

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if os.path.exists('README.rst'):
 
 setup(
     name='kcli',
-    version='1.0.28',
+    version='1.0.29',
     packages=find_packages(),
     include_package_data=True,
     description=description,
