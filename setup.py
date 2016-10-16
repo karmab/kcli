a6639259f32d279fb1c638be3f6c3b4177b6a82c
karimb <karimboumedhel@gmail.com>
2016-10-16 23:32:29 +0200

    ability to create networks within plan file, and treating them first in those cases. new keysword reserveip at profile level to force dhcp reservation, regardless of whether cloudinit is enabled

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if os.path.exists('README.rst'):
 
 setup(
     name='kcli',
-    version='2.0.0',
+    version='2.0',
     packages=find_packages(),
     include_package_data=True,
     description=description,
