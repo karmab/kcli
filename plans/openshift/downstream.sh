486fa3aeb3c86a7dcfbd8b968e274f19a3691425
karimb <karimboumedhel@gmail.com>
2016-11-10 15:16:22 +0100

    allow admin to list pods and nodes

--- plans/openshift/downstream.sh
+++ plans/openshift/downstream.sh
@@ -5,3 +5,4 @@ sed -i "s@# INSECURE_REGISTRY=.*@INSECURE_REGISTRY='--insecure-registry 172.30.0
 systemctl enable docker
 systemctl start docker --ignore-dependencies
 oc cluster up --image=registry.access.redhat.com/openshift3/ose
+oadm policy add-cluster-role-to-user cluster-admin admin --config=/var/lib/origin/openshift.local.config/master/admin.kubeconfig
