6fa6c62cb8adc5a91cd51d52ae863aefdb125814
karimb <karimboumedhel@gmail.com>
2016-12-01 15:43:42 +0100

    use eth1 instead for undercloud

--- plans/openstack/tripleo/undercloud.sh
+++ plans/openstack/tripleo/undercloud.sh
@@ -2,5 +2,5 @@ useradd stack
 echo stack | passwd --stdin stack
 echo "stack ALL=(root) NOPASSWD:ALL" > /etc/sudoers.d/stack
 chmod 0440 /etc/sudoers.d/stack
-yum install -y python-rdomanager-oscplugin wget
+yum install -y python-rdomanager-oscplugin wget vim screen
 su - stack -c "wget https://raw.githubusercontent.com/karmab/kcli/master/plans/openstack/tripleo/undercloud.conf"
