2da6151d23ec19ea254d2293ee0f8e6e0a697a2c
karimb <karimboumedhel@gmail.com>
2016-12-02 18:55:32 +0100

    use eth1 instead for undercloud

--- plans/openstack/tripleo/undercloud.sh
+++ plans/openstack/tripleo/undercloud.sh
@@ -6,8 +6,9 @@ for i in /usr/share/rhosp-director-images/overcloud-full-latest-9.0.tar /usr/sha
 openstack overcloud image upload --image-path /home/stack/images
 neutron subnet-update `neutron subnet-list -c id -f value` --dns-nameserver 8.8.8.8
 ssh-keyscan -H 192.168.101.1 >> ~/.ssh/known_hosts
-##tr '\n' '@' < .ssh/id_rsa  > prout
-##sed -i "s/@/\\\\n/g" prout
+tr '\n' '@' < .ssh/id_rsa  > prout
+sed -i "s/@/\\\\n/g" prout
+# edit damn instackenv.json to put the correct key
 openstack baremetal import --json ~/instackenv.json
 openstack baremetal configure boot
 
