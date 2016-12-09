08f032278df70dbecbf5c52ecd6c0ec0c9888d59
karimb <karimboumedhel@gmail.com>
2016-12-09 16:20:53 +0100

    test for tests

--- tests/test_kvirt.py
+++ tests/test_kvirt.py
@@ -14,7 +14,6 @@ class TestK:
         self.virttype = os.environ.get('KVIRT_TYPE', 'kvm')
         self.libvirt_user = os.environ.get('KVIRT_LIBVIRT_USER', 'qemu')
         k = Kvirt(self.host)
-        print dir(k)
         name = "test_%s" % ''.join(random.choice(string.lowercase) for i in range(5))
         self.name = name
         self.conn = k
@@ -35,37 +34,36 @@ class TestK:
         k.create_pool(name=self.name, poolpath='%s/%s' % (self.path, self.name), user=self.libvirt_user)
         assert True
 
-#    def test_create_vm(self):
-#        k = self.conn
-#        time.sleep(10)
-#        print self.virttype
-#        k.create(self.name, virttype=self.virttype, numcpus=1, memory=512, pool=self.name, nets=[self.name])
-#        status = k.status(self.name)
-#        print(status)
-#        assert status is not None
+    def test_create_vm(self):
+        k = self.conn
+        time.sleep(10)
+        k.create(self.name, virttype=self.virttype, numcpus=1, memory=512, pool=self.name, nets=[self.name])
+        status = k.status(self.name)
+        print(status)
+        assert status is not None
 
-#    def test_add_disk(self):
-#        k = self.conn
-#        k.add_disk(name=self.name, size=1, pool=self.name)
-#        assert True
-#
-#    def test_stop_vm(self):
-#        k = self.conn
-#        k.stop(self.name)
-#        status = k.status(self.name)
-#        assert status == 'down'
-#
-#    def test_start_vm(self):
-#        k = self.conn
-#        k.start(self.name)
-#        status = k.status(self.name)
-#        assert status == 'up'
-#
-#    def test_delete_vm(self):
-#        k = self.conn
-#        k.delete(self.name)
-#        status = k.status(self.name)
-#        assert status is None
+    def test_add_disk(self):
+        k = self.conn
+        k.add_disk(name=self.name, size=1, pool=self.name)
+        assert True
+
+    def test_stop_vm(self):
+        k = self.conn
+        k.stop(self.name)
+        status = k.status(self.name)
+        assert status == 'down'
+
+    def test_start_vm(self):
+        k = self.conn
+        k.start(self.name)
+        status = k.status(self.name)
+        assert status == 'up'
+
+    def test_delete_vm(self):
+        k = self.conn
+        k.delete(self.name)
+        status = k.status(self.name)
+        assert status is None
 
     @classmethod
     def teardown_class(self):
