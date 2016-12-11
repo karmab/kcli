6b52654ac24a9718ba305d46f6d41024be4a8e56
karimb <karimboumedhel@gmail.com>
2016-12-12 00:49:38 +0100

    helpers for tripleo postconfig and correct hostname assignation

--- plans/openstack/tripleo/advanced/assignprofiles.sh
+++ plans/openstack/tripleo/advanced/assignprofiles.sh
@@ -3,10 +3,12 @@
 uuids=`ironic node-list | grep -v UUID | awk -F'|' '{ print $2}' | xargs`
 counter=0
 for uuid in $uuids ; do
-counter=$(( counter +1))
-  if [ $counter -eq 4 ] ; then
-    ironic node-update $uuid add properties/capabilities='profile:compute,boot_option:local'
+  if [ $counter -eq 3 ] ; then
+    #ironic node-update $uuid add properties/capabilities='profile:compute,boot_option:local'
+    ironic node-update $uuid add properties/capabilities="node:compute-0,profile:compute,boot_option:local"
   else
-    ironic node-update $uuid add properties/capabilities='profile:control,boot_option:local'
+    #ironic node-update $uuid add properties/capabilities='profile:control,boot_option:local'
+    ironic node-update $uuid add properties/capabilities="node:controller-$counter,profile:control,boot_option:local"
   fi
+counter=$(( counter +1))
 done
---
+++ plans/openstack/tripleo/templates/environments/postconfig.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo i am the king of the bongo >> /tmp/prout
