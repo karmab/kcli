69b2d1a38b0449069c8b502c241772234e94358a
karimb <karimboumedhel@gmail.com>
2016-12-23 22:05:12 +0100

    tripleo fixes

--- plans/openstack/tripleo/newton.sh
+++ plans/openstack/tripleo/newton.sh
@@ -1,1 +1,1 @@
-subscription-manager repos --enable=rhel-7-server-rh-common-rpms --enable=rhel-7-server-openstack-9-rpms --enable=rhel-ha-for-rhel-7-server-rpms --enable=rhel-7-server-extras-rpms --enable rhel-7-server-openstack-10-director-rpms
+subscription-manager repos --enable=rhel-7-server-rh-common-rpms --enable=rhel-7-server-openstack-10-rpms --enable=rhel-ha-for-rhel-7-server-rpms --enable=rhel-7-server-extras-rpms --enable rhel-7-server-openstack-10-director-rpms
