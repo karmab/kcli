88f47b30e82bf91a49395323aee8055beb4e2a1d
karimb <karimboumedhel@gmail.com>
2016-12-25 20:28:32 +0100

    skip comments when creating cloudinit commands part

--- plans/openstack/tripleo/README.md
+++ plans/openstack/tripleo/README.md
@@ -1,8 +1,8 @@
 
+# important to have the provisioning network without nat
+# also on the hypervisor 
+echo 0 > /proc/sys/net/ipv4/conf/all/rp_filter
+echo 0 > /proc/sys/net/ipv4/conf/default/rp_filter
 # openstack undercloud install cant be launched from cloudinit , as a tty is required (for sudo). investigate whether disabling requiretty is enough
 # you will need http://mirror.centos.org/centos/7/cloud/x86_64/openstack-mitaka/common/ipxe-roms-qemu-20160127-1.git6366fa7a.el7.noarch.rpm if using a centos hypervisor
 # need to launch openstack undercloud install twice as default gateway disappears. should investigate whehther ifcfg-eth1  file misses something ( BOOTPROTO set to static ? )
-# i add to add an extra iptables rule cos the default one  didnt work
--A PREROUTING -d 169.254.169.254/32 -i br-ctlplane -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 8775
-instead i used
-iptables -t nat -A PREROUTING -d 169.254.169.254/32 -s 10.0.0.0/24 -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 8775
--- plans/openstack/tripleo/undercloud.sh
+++ plans/openstack/tripleo/undercloud.sh
@@ -1,14 +1,8 @@
 VERSION="10"
 ssh-keygen -N '' -t rsa -f /home/stack/.ssh/id_rsa
 ssh-copy-id -i ~/.ssh/id_rsa.pub root@192.168.101.1
-yum clean all
 screen openstack undercloud install
 mkdir images
-#mkdir -p templates/environments
-#cd templates/environments
-#wget https://raw.githubusercontent.com/karmab/kcli/master/plans/openstack/tripleo/environments/password.yaml
-#wget https://raw.githubusercontent.com/karmab/kcli/master/plans/openstack/tripleo/environments/_password.yaml
-#wget https://raw.githubusercontent.com/karmab/kcli/master/plans/openstack/tripleo/environments/password.sh
 wget https://raw.githubusercontent.com/karmab/kcli/master/plans/openstack/tripleo/templates.tar.gz
 tar zxvf templates.tar.gz
 cd ~/images
@@ -19,16 +13,9 @@ neutron subnet-update `neutron subnet-list -c id -f value` --dns-nameserver 8.8.
 ssh-keyscan -H 192.168.101.1 >> ~/.ssh/known_hosts
 cd
 sh instackenv.sh
-#tr '\n' '@' < .ssh/id_rsa  > prout
-#sed -i "s/@/\\\\n/g" prout
 openstack baremetal import --json ~/instackenv.json
 sleep 3
 openstack baremetal configure boot
 openstack baremetal introspection bulk start
 sh assignprofiles.sh
-#openstack overcloud deploy --templates
-
-# ISSUES FOUND
-# you will need http://mirror.centos.org/centos/7/cloud/x86_64/openstack-mitaka/common/ipxe-roms-qemu-20160127-1.git6366fa7a.el7.noarch.rpm if using a centos hypervisor
-# on newton, i had to make some adjustements:
-# set interface_driver to neutron.agent.linux.interface.OVSInterfaceDriver on metadata__agent.ini  for service to start
+sh deploy.sh
