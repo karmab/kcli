3294e00c0dc44d778117a23fc51922737a9cd013
karimb <karimboumedhel@gmail.com>
2017-02-21 17:38:24 +0100

    handle symlinks for kcli get

--- kvirt/plansutils.py
+++ kvirt/plansutils.py
@@ -5,6 +5,24 @@ import json
 import os
 
 
+def symlinks(user, repo):
+    mappings = []
+    url1 = 'https://api.github.com/repos/%s/%s/git/refs/heads/master' % (user, repo)
+    r = urllib2.urlopen(url1)
+    base = json.load(r)
+    sha = base['object']['sha']
+    url2 = 'https://api.github.com/repos/%s/%s/git/trees/%s?recursive=1' % (user, repo, sha)
+    r = urllib2.urlopen(url2)
+    try:
+        base = json.load(r)
+    except:
+        return []
+    for e in base['tree']:
+        if e['mode'] == '120000':
+            mappings.append(e['path'])
+    return mappings
+
+
 def download(url, path):
     filename = os.path.basename(url)
     print("Fetching %s" % filename)
@@ -13,7 +31,15 @@ def download(url, path):
         output.write(url.read())
 
 
-def fetch(url, path):
+def makelink(url, path):
+    filename = os.path.basename(url)
+    url = urllib2.urlopen(url)
+    target = url.read()
+    print("Creating symlink for %s pointing to %s" % (filename, target))
+    os.symlink(target, "%s/%s" % (path, filename))
+
+
+def fetch(url, path, syms=None):
     if not url.startswith('http'):
         url = "https://%s" % url
     if 'github.com' not in url or 'raw.githubusercontent.com' in url:
@@ -25,6 +51,7 @@ def fetch(url, path):
         tempurl = url.replace('https://api.github.com/repos/', '')
         user = tempurl.split('/')[0]
         repo = tempurl.split('/')[1]
+        syms = symlinks(user, repo)
         url = url.replace("%s/%s" % (user, repo), "%s/%s/contents" % (user, repo))
     if not os.path.exists(path):
         os.mkdir(path)
@@ -40,8 +67,11 @@ def fetch(url, path):
             os._exit(1)
         filename = b['name']
         filetype = b['type']
+        filepath = b['path']
         download_url = b['download_url']
-        if filetype == 'file':
+        if filepath in syms:
+            makelink(download_url, path)
+        elif filetype == 'file':
             download(download_url, path)
         elif filetype == 'dir':
-            fetch("%s/%s" % (url, filename), "%s/%s" % (path, filename))
+            fetch("%s/%s" % (url, filename), "%s/%s" % (path, filename), syms=syms)
--- plans/openstack/subscription_kilo.sh
+++ plans/openstack/subscription_kilo.sh
@@ -1,14 +1,1 @@
-export ADMIN_PASSWORD="unix1234"
-echo `hostname -I` `hostname -s` >> /etc/hosts
 subscription-manager repos --enable=rhel-7-server-rh-common-rpms --enable=rhel-7-server-openstack-7.0-rpms --enable=rhel-ha-for-rhel-7-server-rpms --enable=rhel-7-server-extras-rpms
-yum update -y
-yum install -y openstack-packstack wget vim screen
-HOME=/root packstack --gen-answer-file=/root/answers.txt
-sed -i "s/CONFIG_SWIFT_INSTALL=y/CONFIG_SWIFT_INSTALL=n/" /root/answers.txt
-sed -i "s/CONFIG_HEAT_INSTALL=n/CONFIG_HEAT_INSTALL=y/" /root/answers.txt
-sed -i "s/CONFIG_NAGIOS_INSTALL=y/CONFIG_NAGIOS_INSTALL=n/" /root/answers.txt
-sed -i "s/CONFIG_PROVISION_DEMO=y/CONFIG_PROVISION_DEMO=n/" /root/answers.txt
-sed -i "s/CONFIG_SWIFT_INSTALL=y/CONFIG_SWIFT_INSTALL=n/" /root/answers.txt
-sed -i "s/CONFIG_NEUTRON_OVS_BRIDGE_MAPPINGS=/CONFIG_NEUTRON_OVS_BRIDGE_MAPPINGS=external:br-ex/" /root/answers.txt
-sed -i "s/CONFIG_NEUTRON_OVS_BRIDGE_IFACES=/CONFIG_NEUTRON_OVS_BRIDGE_IFACES=br-ex:eth0/" /root/answers.txt
-HOME=/root packstack --answer-file=/root/answers.txt
