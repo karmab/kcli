86e794d8e837a8705f861858c46618d5866505b1
karimb <karimboumedhel@gmail.com>
2017-04-15 00:20:13 +0200

    UI hosts tab with list/switch/enable/disable

---
+++ kvirt/static/js/hostactions.js
@@ -0,0 +1,17 @@
+function hostaction(client, action){
+  $("#wheel").show();
+  data = {'name': client, 'action': action};
+  $.ajax({  
+       type: "POST",
+        url: '/hostaction',
+        data: data,
+        success: function(data) {
+            $("#wheel").hide();
+            if (data.result == 'success') {
+                $('.top-right').notify({message: { text: "Action "+action+" succeeded!!!" }, type: 'success'}).show(); 
+            } else {
+                $('.top-right').notify({message: { text: "Action "+action+" Failed because of "+data.reason }, type: 'danger'}).show(); 
+            };
+		}
+	});
+}
