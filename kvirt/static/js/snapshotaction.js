b8c93634838f9d398e06e848664f13a94a3cea71
karimb <karimboumedhel@gmail.com>
2017-04-16 02:33:21 +0200

    UI snapshot delete/revert

--- kvirt/static/js/snapshotaction.js
+++ kvirt/static/js/snapshotaction.js
@@ -63,7 +63,7 @@ function snapshotrevert(name){
         data: data,
         success: function(snapshots) {
             if (snapshots.length == 0) {
-                $('.top-right').notify({message: { text: "No snapshots found" }, type: 'success'}).show();
+                $('.top-right').notify({message: { text: "No snapshots found" }, type: 'danger'}).show();
                 return
             } else {
                 var snapshot = prompt("Choose snapshots between the following ones:\n"+snapshots);
