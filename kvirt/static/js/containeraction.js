5579c3a1ae47494892fd00250b4992489e229d79
karimb <karimboumedhel@gmail.com>
2017-04-17 12:31:23 +0200

    Fix UI container delete javascript

--- kvirt/static/js/containeraction.js
+++ kvirt/static/js/containeraction.js
@@ -43,10 +43,10 @@ function containerdelete(container){
         data: data,
         success: function(data) {
             $("#wheel").hide();
-            if (data == '0') {
+            if (data.result == 'success') {
                 $('.top-right').notify({message: { text: "Container "+container+" Deleted!!!" }, type: 'success'}).show();
             } else {
-                $('.top-right').notify({message: { text: "Container "+container+" Failed to Delete" }, type: 'danger'}).show();
+                $('.top-right').notify({message: { text: "Container "+container+" Failed to Delete because "+data.reason }, type: 'danger'}).show();
             };
         }
     });
