657a47e93aaa9d32d5519f5327b526632ff85d24
karimb <karimboumedhel@gmail.com>
2017-04-20 01:54:56 +0200

    fix timeout with ovirt hosted plan

--- plans/ovirt/hosted/bootstrap.sh
+++ plans/ovirt/hosted/bootstrap.sh
@@ -1,7 +1,7 @@
 yum -y install rhevm-appliance screen ovirt-hosted-engine-setup ovirt-engine-cli
 sed -i /requiretty/d /etc/sudoers
 hosted-engine --deploy --config-append=/root/answers.conf
-sleep 60
+sleep 300
 hostnode="rhvnode1.default"
 nfsnode="rhvnfs.default"
 ovirt-shell -E "add storagedomain --name vms --host-name $hostnode --type data --storage-type nfs --storage-address $nfsnode --storage-path /vms"
