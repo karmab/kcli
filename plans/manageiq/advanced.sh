4855fc562c47db9fa3841f4aa0897334d1bd4015
karimb <karimboumedhel@gmail.com>
2017-04-22 19:06:46 +0200

    advanced manageiq plan

---
+++ plans/manageiq/advanced.sh
@@ -0,0 +1,8 @@
+echo unix1234 | passwd --stdin root
+source /etc/profile.d/evm.sh
+appliance_console_cli --host=cloudforms --region=01 --internal --password="unix1234" --key --force-key
+sleep 60
+/var/www/miq/vmdb/bin/rails r /root/password.rb
+python /root/openstack.py
+python /root/rhev.py
+python /root/openshift.py
