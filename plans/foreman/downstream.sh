878b5be4468164ff56e10f7de8557f1489268247
karimb <karimboumedhel@gmail.com>
2017-05-09 13:54:27 +0200

    fix satellite downstream plan

--- plans/foreman/downstream.sh
+++ plans/foreman/downstream.sh
@@ -4,6 +4,6 @@ export PASSWORD="unix1234"
 echo `hostname -I` `hostname -s`.default `hostname -s` >> /etc/hosts
 echo `hostname -s`.default > /etc/hostname
 # yum -y update
-yum -y install satellite-installer wget
+yum -y install satellite wget
 #reboot
 satellite-installer --scenario satellite --foreman-admin-username admin  --foreman-admin-password $PASSWORD --foreman-initial-location $LOCATION --foreman-initial-organization $ORG --capsule-puppet true --foreman-proxy-puppetca true --foreman-proxy-tftp true --enable-foreman-plugin-discovery
