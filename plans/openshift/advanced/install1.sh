a2fc32c1f1a45d5c0b682d8cf2f564d3f2171817
karimb <karimboumedhel@gmail.com>
2017-07-03 22:20:38 +0200

    cleaner openshift advanced plan

--- plans/openshift/advanced/install1.sh
+++
@@ -1,8 +0,0 @@
-yum -y install openshift-ansible-playbooks
-ssh-keyscan -H lb.karmalabs.local >> ~/.ssh/known_hosts
-ssh-keyscan -H master01.karmalabs.local >> ~/.ssh/known_hosts
-ssh-keyscan -H master02.karmalabs.local >> ~/.ssh/known_hosts
-ssh-keyscan -H master03.karmalabs.local >> ~/.ssh/known_hosts
-ssh-keyscan -H node01.karmalabs.local >> ~/.ssh/known_hosts
-ssh-keyscan -H node02.karmalabs.local >> ~/.ssh/known_hosts
-ssh-keyscan -H node03.karmalabs.local >> ~/.ssh/known_hosts
--- plans/openshift/advanced/install2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-ansible-playbook -i /root/hosts /usr/share/ansible/openshift-ansible/playbooks/byo/config.yml
-for i in 1 2 3 ; do 
-  ssh master${i}.karmalabs.local "htpasswd -b /etc/origin/master/htpasswd karim karim"
-done
