1e0758e9340433578fd2ba601344d494052fb8f7
karimb <karimboumedhel@gmail.com>
2017-07-14 01:16:26 +0200

    updated openshift upstream plan to 3.6

--- plans/openshift/upstream.sh
+++ plans/openshift/upstream.sh
@@ -1,3 +1,4 @@
 systemctl start docker --ignore-dependencies
-oc cluster up
-oadm policy add-cluster-role-to-user cluster-admin admin --config=/var/lib/origin/openshift.local.config/master/admin.kubeconfig
+export IP=`ip a l  eth0 | grep 'inet ' | cut -d' ' -f6 | awk -F'/' '{ print $1}'`
+oc cluster up --public-hostname $IP.xip.io
+docker exec -it origin oadm policy add-cluster-role-to-user cluster-admin admin --config=/var/lib/origin/openshift.local.config/master/admin.kubeconfig
