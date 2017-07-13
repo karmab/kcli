0975675fda52aeaeba82f655837bbee1b85b6968
karimb <karimboumedhel@gmail.com>
2017-07-13 23:53:12 +0200

    updated openshift upstream plan to 3.6

--- plans/openshift/install.sh
+++ plans/openshift/install.sh
@@ -3,7 +3,7 @@ systemctl enable docker
 sed -i "s@# INSECURE_REGISTRY=.*@INSECURE_REGISTRY='--insecure-registry 172.30.0.0/16'@" /etc/sysconfig/docker
 echo -e "DEVS=/dev/vdb\nVG=dockervg" > /etc/sysconfig/docker-storage-setup
 docker-storage-setup
-waet -O /root/oc.tar.gz https://github.com/openshift/origin/releases/download/v1.5.1/openshift-origin-client-tools-v1.5.1-7b451fc-linux-64bit.tar.gz
+wget -O /root/oc.tar.gz https://github.com/openshift/origin/releases/download/v3.6.0-alpha.2/openshift-origin-client-tools-v3.6.0-alpha.2-3c221d5-linux-64bit.tar.gz
 cd /root ; tar zxvf oc.tar.gz
-mv /root/openshift-origin-client-tools-v1.5.1-7b451fc-linux-64bit/oc /usr/bin
+mv /root/openshift-origin-client-tools-v3.6.0-alpha.2-3c221d5-linux-64bit/oc /usr/bin
 rm -rf  /root/openshift*
