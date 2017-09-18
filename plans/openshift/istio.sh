ba0a2682a391aea1c765de51b8a579c0d719aa7b
karimb <karimboumedhel@gmail.com>
2017-09-18 17:38:38 +0200

    istio preliminary plans

---
+++ plans/openshift/istio.sh
@@ -0,0 +1,36 @@
+export KUBECONFIG=/root/.kube/config
+git clone https://github.com/istio/istio.git /root
+cd /root/istio
+git checkout $checkout
+oc login -u system:admin
+oc new-project $project
+oc adm policy add-scc-to-user anyuid -z default
+oc adm policy add-scc-to-user privileged -z default
+oc adm policy add-cluster-role-to-user cluster-admin -z default
+oc adm policy add-cluster-role-to-user cluster-admin -z istio-pilot-service-account
+oc adm policy add-cluster-role-to-user cluster-admin -z istio-ingress-service-account
+oc adm policy add-cluster-role-to-user cluster-admin -z istio-egress-service-account
+oc adm policy add-cluster-role-to-user cluster-admin -z istio-mixer-service-account
+oc adm policy add-scc-to-user anyuid -z istio-ingress-service-account
+oc adm policy add-scc-to-user privileged -z istio-ingress-service-account
+oc adm policy add-scc-to-user anyuid -z istio-egress-service-account
+oc adm policy add-scc-to-user privileged -z istio-egress-service-account
+oc adm policy add-scc-to-user anyuid -z istio-pilot-service-account
+oc adm policy add-scc-to-user privileged -z istio-pilot-service-account
+oc create -f install/kubernetes/istio-rbac-beta.yaml
+oc create -f install/kubernetes/istio.yaml
+oc expose svc istio-ingress
+sleep 60
+oc apply -f install/kubernetes/addons/prometheus.yaml
+oc apply -f install/kubernetes/addons/grafana.yaml
+oc apply -f install/kubernetes/addons/servicegraph.yaml
+oc expose svc servicegraph
+oc expose svc grafana
+sleep 60
+curl -LO https://github.com/istio/istio/releases/download/$version/istio-$version-linux.tar.gz
+tar zxvf istio-$version-linux.tar.gz
+sudo mv istio-$version/bin/istioctl /usr/bin
+sudo chmod u+x /usr/bin/istioctl
+source istio.VERSION
+oc apply -f <(istioctl kube-inject --hub $PILOT_HUB --tag $PILOT_TAG -f samples/apps/bookinfo/bookinfo.yaml -n $project)
+oc expose svc productpage
--- plans/openshift/istio21.sh
+++ plans/openshift/istio21.sh
@@ -1,39 +1,3 @@
 export version="0.2.1"
 export checkout="103b3dd4c74a03d226ca5cf1b1ed9951ab8707be"
 export project="myproject"
-
-git clone https://github.com/istio/istio.git
-cd istio
-git checkout $checkout
-oc login -u system:admin
-oc new-project $project
-oc adm policy add-scc-to-user anyuid -z default
-oc adm policy add-scc-to-user privileged -z default
-oc adm policy add-cluster-role-to-user cluster-admin -z default
-oc adm policy add-cluster-role-to-user cluster-admin -z istio-pilot-service-account
-oc adm policy add-cluster-role-to-user cluster-admin -z istio-ingress-service-account
-oc adm policy add-cluster-role-to-user cluster-admin -z istio-egress-service-account
-oc adm policy add-cluster-role-to-user cluster-admin -z istio-mixer-service-account
-oc adm policy add-scc-to-user anyuid -z istio-ingress-service-account
-oc adm policy add-scc-to-user privileged -z istio-ingress-service-account
-oc adm policy add-scc-to-user anyuid -z istio-egress-service-account
-oc adm policy add-scc-to-user privileged -z istio-egress-service-account
-oc adm policy add-scc-to-user anyuid -z istio-pilot-service-account
-oc adm policy add-scc-to-user privileged -z istio-pilot-service-account
-oc create -f install/kubernetes/istio-rbac-beta.yaml
-oc create -f install/kubernetes/istio.yaml
-oc expose svc istio-ingress
-sleep 20
-oc apply -f install/kubernetes/addons/prometheus.yaml
-oc apply -f install/kubernetes/addons/grafana.yaml
-oc apply -f install/kubernetes/addons/servicegraph.yaml
-oc expose svc servicegraph
-oc expose svc grafana
-curl -LO https://github.com/istio/istio/releases/download/$version/istio-$version-linux.tar.gz
-tar zxvf istio-$version-linux.tar.gz
-sudo mv istio-$version/bin/istioctl /usr/bin
-sudo chmod u+x /usr/bin/istioctl
-sleep 20
-source istio.VERSION
-oc apply -f <(istioctl kube-inject --hub $PILOT_HUB --tag $PILOT_TAG -f samples/apps/bookinfo/bookinfo.yaml -n myproject)
-oc expose svc productpage
