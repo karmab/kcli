6e4cd9977b2defb716a8153676162d809cb2bcd5
karimb <karimboumedhel@gmail.com>
2017-10-10 19:18:37 +0200

    helm plan

--- plans/openshift/helm.sh
+++ plans/openshift/helm.sh
@@ -1,10 +1,12 @@
 oc login -u system:admin
 oc new-project helm
 oc create serviceaccount helm --namespace=helm
-docker exec -it origin oadm policy add-cluster-role-to-user cluster-admin system:serviceaccount:helm:helm
+docker exec origin oadm policy add-cluster-role-to-user cluster-admin system:serviceaccount:helm:helm
 curl -LO https://kubernetes-helm.storage.googleapis.com/helm-v2.6.0-linux-amd64.tar.gz
 tar zxvf helm-v2.6.0-linux-amd64.tar.gz
 mv linux-amd64/helm /usr/bin
 chmod u+x /usr/bin/helm
+cd /root
 helm init --service-account helm --tiller-namespace helm
 echo "export TILLER_NAMESPACE=helm" >>/root/.bashrc
+echo "export HELM_HOME=/root/.helm" >>/root/.bashrc
