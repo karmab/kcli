0774c85d3c6c65f4042a1d4145aaea5cc9ebefb9
karimb <karimboumedhel@gmail.com>
2017-10-13 10:59:54 +0200

    fission plan

--- plans/openshift/fission.sh
+++ plans/openshift/fission.sh
@@ -1,5 +1,6 @@
 oc new-project fission
-helm install --namespace fission https://github.com/fission/fission/releases/download/v0.2.1/fission-core-v0.2.1.tgz
-curl -Lo fission https://github.com/fission/fission/releases/download/v0.2.1/fission-cli-linux && chmod +x fission && sudo mv fission /usr/bin/
-expose svc controller
-expose svc router
+export TILLER_NAMESPACE=helm
+helm install --namespace fission https://github.com/fission/fission/releases/download/0.3.0/fission-core-0.3.0.tgz
+curl -Lo fission https://github.com/fission/fission/releases/download/0.3.0/fission-cli-linux && chmod +x fission && mv fission /usr/bin/
+oc expose svc controller
+oc expose svc router
