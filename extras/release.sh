78ffa276771c6cb76d50d068eebe6f7244e27851
karimb <karimboumedhel@gmail.com>
2017-10-20 21:14:21 +0200

    adjustments on release.sh

--- extras/release.sh
+++ extras/release.sh
@@ -8,8 +8,9 @@
 # change version in relevant files(setup.py, kvirt/config.py and extra/kcli.spec)
 # commit and push code
 # generate python package
-# generate release with the corresponding changelog
+# generate release without the changelog :( and using RELEASETOKEN env variable
 # launch build of rpm and deb packages (using kcli product)
+# displays the changelog so it can be added manually
 
 cd $HOME/CODE/git/KARIM/kcli
 USER="karmab"
@@ -40,7 +41,7 @@ echo -n $CHANGELOG
 
 # SET NEW VERSIONS
 sed -i s"/    version.*/    version='$VERSION',/" setup.py
-sed "s/Version:.*/Version:        $VERSION/"  ~/kcli.spec
+sed -i "s/Version:.*/Version:        $VERSION/"  ~/kcli.spec
 sed -i s"/__version__.*/__version__ = '$VERSION'/" kvirt/config.py
 
 # PUSH CODE 
@@ -51,8 +52,16 @@ git push
 python setup.py sdist upload
 
 # GENERATE RELEASE
-API_JSON=$(printf '{"tag_name": "v%s","target_commitish": "master","name": "v%s","body": "%s","draft": false,"prerelease": false}' $VERSION $VERSION $CHANGELOG)
+# API_JSON=$(printf '{"tag_name": "v%s","target_commitish": "master","name": "v%s","body": "%s","draft": false,"prerelease": false}' $VERSION $VERSION $CHANGELOG)
+API_JSON=$(printf '{"tag_name": "v%s","target_commitish": "master","name": "v%s","body": "Release of version %s","draft": false,"prerelease": false}' $VERSION $VERSION $CHANGELOG)
 curl --data "$API_JSON" https://api.github.com/repos/$USER/$REPOSITORY/releases?access_token=$RELEASETOKEN
 
 # GENERATE RPM/DEB
-kcli product copr
+kcli delete --yes copr
+kcli product -c copr
+
+echo "This is the changelog:"
+echo 
+echo $CHANGELOG
+
+echo https://github.com/karmab/kcli/releases/edit/v$VERSION
