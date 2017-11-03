0dae74991ae242576454a346b307b8bbe68d96b2
karimb <karimboumedhel@gmail.com>
2017-11-03 17:04:49 +0100

    old openshift releases plans

--- plans/openshift/requirements36.sh
+++ plans/openshift/requirements36.sh
@@ -3,7 +3,7 @@ systemctl enable docker
 sed -i "s@# INSECURE_REGISTRY=.*@INSECURE_REGISTRY='--insecure-registry 172.30.0.0/16'@" /etc/sysconfig/docker
 echo -e "DEVS=/dev/vdb\nVG=dockervg" > /etc/sysconfig/docker-storage-setup
 docker-storage-setup
-wget -O /root/oc.tar.gz https://github.com/openshift/origin/releases/download/v3.6.0/openshift-origin-client-tools-v3.6.0-c4dd4cf-linux-64bit.tar.gz
+wget -O /root/oc.tar.gz https://github.com/openshift/origin/releases/download/v3.6.1/openshift-origin-client-tools-v3.6.1-008f2d5-linux-64bit.tar.gz
 cd /root ; tar zxvf oc.tar.gz
 mv /root/openshift-origin-client-tools-*/oc /usr/bin
 rm -rf  /root/openshift*
