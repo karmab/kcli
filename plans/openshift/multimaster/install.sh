8614a81266c7bd895dc5cb9f95a413786456831d
Jorge Tudela <jtudelag@users.noreply.github.com>
2017-11-11 19:42:25 +0100

    Enables Ansible logs OCP multimaster plan
    When installing OCP using the multimaster plan, Ansible logs are lost.
    
    Enabling them on /etc/ansible/ansible.cfg, so in case of failure during the installation,
    there is a log to check.
    
    By default to /var/log/ansible.log.

--- plans/openshift/multimaster/install.sh
+++ plans/openshift/multimaster/install.sh
@@ -8,6 +8,7 @@ ssh-keyscan -H master03.karmalabs.local >> ~/.ssh/known_hosts
 ssh-keyscan -H node01.karmalabs.local >> ~/.ssh/known_hosts
 ssh-keyscan -H node02.karmalabs.local >> ~/.ssh/known_hosts
 export IP=`ip a l  eth0 | grep 'inet ' | cut -d' ' -f6 | awk -F'/' '{ print $1}'`
+sed -i "s/#log_path/log_path/" /etc/ansible/ansible.cfg
 sed -i "s/openshift_master_default_subdomain=.*/openshift_master_default_subdomain=$IP.xip.io/" /root/hosts
 ansible-playbook -i /root/hosts /usr/share/ansible/openshift-ansible/playbooks/byo/config.yml
 for i in 1 2 3 ; do 
