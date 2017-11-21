3e720d4e7d6c349b5620126d3cb4f69c93d1f2d8
karimb <karimboumedhel@gmail.com>
2017-11-21 14:54:01 +0100

    workaround for asb issues

--- plans/openshift/asb.sh
+++ plans/openshift/asb.sh
@@ -6,11 +6,15 @@ sleep 20
 dnf -y install dnf-plugins-core
 dnf -y copr enable @ansible-service-broker/ansible-service-broker
 dnf -y install apb
-wget https://apb-oc.s3.amazonaws.com/apb-oc/oc-linux-64bit.tar.gz
-tar zxvf oc-linux-64bit.tar.gz
-mv oc-linux-64bit/oc /usr/bin
-chmod u+x /usr/bin/oc
+#wget https://s3.amazonaws.com/catasb/linux/amd64/oc
+#mv oc /usr/bin
+#wget https://apb-oc.s3.amazonaws.com/apb-oc/oc-linux-64bit.tar.gz
+#tar zxvf oc-linux-64bit.tar.gz
+#mv oc-linux-64bit/oc /usr/bin
+#chmod u+x /usr/bin/oc
 wget https://raw.githubusercontent.com/openshift/ansible-service-broker/master/scripts/run_latest_build.sh
+export ORIGIN_VERSION="v3.7.0-rc.0"
+rm -rf /usr/share/rhel/secrets
 chmod +x run_latest_build.sh
 export PUBLIC_IP=`ip a l  eth0 | grep 'inet ' | cut -d' ' -f6 | awk -F'/' '{ print $1}'`
 sh run_latest_build.sh
