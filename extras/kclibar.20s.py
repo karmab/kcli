b52634e741d3c5f7eb81e6c86728984807eb08e1
karimb <karimboumedhel@gmail.com>
2017-11-24 10:31:40 +0100

    bitbar extra

---
+++ extras/kclibar.20s.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# <bitbar.title>kcli list</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>Karim Boumedhel</bitbar.author>
+# <bitbar.author.github>karmab</bitbar.author.github>
+# <bitbar.desc>Lists running vms using kcli</bitbar.desc>
+# <bitbar.image>https://raw.githubusercontent.com/karmab/kcli/master/kcli.jpg</bitbar.image>
+# <bitbar.abouturl>https://github.com/karmab/kcli</bitbar.abouturl>
+
+import sys
+try:
+    from kvirt.config import Kconfig
+except:
+    warning = u"\u26A0"
+    warning = warning.encode('utf-8')
+    print("%s\n" % warning)
+    print("---\n")
+    print("Kcli could not be found in paths. Is it installed?")
+    sys.exit(1)
+
+config = Kconfig(quiet=True)
+k = config.k
+vms = k.list()
+running = [vm[0] for vm in vms if vm[1] == 'up']
+off = [vm[0] for vm in vms if vm[1] == 'down']
+for vm in running:
+    print("%s| color=green" % vm)
+    # print("%s| color=green bash=kcli param1=ssh param2=%s" % (vm, vm))
+print('---')
+for vm in off:
+    print(vm)
