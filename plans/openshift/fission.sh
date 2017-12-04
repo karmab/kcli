00316db8d2a32766d1f8c41f2d8d0e0182659cea
karimb <karimboumedhel@gmail.com>
2017-12-04 17:06:39 +0100

    fission 0.4.0 plan

--- plans/openshift/fission.sh
+++ plans/openshift/fission.sh
@@ -1,7 +1,7 @@
 oc new-project fission
 export TILLER_NAMESPACE=helm
 oc adm policy add-cluster-role-to-user cluster-admin -z fission-svc
-helm install --namespace fission https://github.com/fission/fission/releases/download/0.3.0/fission-core-0.3.0.tgz
-curl -Lo fission https://github.com/fission/fission/releases/download/0.3.0/fission-cli-linux && chmod +x fission && mv fission /usr/bin/
+helm install --namespace fission https://github.com/fission/fission/releases/download/0.4.0/fission-core-0.4.0.tgz
+curl -Lo fission https://github.com/fission/fission/releases/download/0.4.0/fission-cli-linux && chmod +x fission && mv fission /usr/bin/
 oc expose svc controller
 oc expose svc router
