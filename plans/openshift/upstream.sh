8cbcb9d6afde1dbd695b92ec7df13d8dea272c60
karimb <karimboumedhel@gmail.com>
2017-12-11 19:29:28 +0100

    more jinja fun

--- plans/openshift/upstream.sh
+++ plans/openshift/upstream.sh
@@ -1,5 +1,9 @@
 sed -i '/OPTIONS=.*/c\OPTIONS="--selinux-enabled --insecure-registry 172.30.0.0/16"' /etc/sysconfig/docker
 systemctl start docker --ignore-dependencies
+[% if 'Fedora' in template %]
+sleep 120
+rm -rf /usr/share/rhel/secrets
+[% endif %]
 export IP=`ip a l  eth0 | grep 'inet ' | cut -d' ' -f6 | awk -F'/' '{ print $1}'`
 oc cluster up --public-hostname $IP.xip.io --routing-suffix $IP.xip.io
 docker exec origin oadm policy add-cluster-role-to-user cluster-admin admin --config=/var/lib/origin/openshift.local.config/master/admin.kubeconfig
