1682723a986b9ae8b0bb855d577cbffc1d175277
karimb <karimboumedhel@gmail.com>
2017-12-13 21:47:45 +0100

    updated allinone openshift plan

--- plans/openshift/allinone/downstream.sh
+++ plans/openshift/allinone/downstream.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yum -y install ansible openshift-ansible-playbooks
-ssh-keyscan -H allinone.karmalabs.local >> ~/.ssh/known_hosts
+ssh-keyscan -H allinone.default >> ~/.ssh/known_hosts
 export IP=`ip a l  eth0 | grep 'inet ' | cut -d' ' -f6 | awk -F'/' '{ print $1}'`
 sed -i "s/openshift_master_default_subdomain=.*/openshift_master_default_subdomain=$IP.xip.io/" /root/hosts
 ansible-playbook -i /root/hosts /usr/share/ansible/openshift-ansible/playbooks/byo/config.yml
