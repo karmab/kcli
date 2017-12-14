8e04ed2575e40c709003314cedde0e3fe96bbd6e
karimb <karimboumedhel@gmail.com>
2017-12-14 17:07:18 +0100

    added kubevirt plan and tweaked fission one

--- plans/openshift/kubevirt.sh
+++ plans/openshift/kubevirt.sh
@@ -1,7 +1,17 @@
-sed -i '/OPTIONS=.*/c\OPTIONS="--selinux-enabled --insecure-registry 172.30.0.0/16"' /etc/sysconfig/docker
-systemctl start docker --ignore-dependencies
-export IP=`ip a l  eth0 | grep 'inet ' | cut -d' ' -f6 | awk -F'/' '{ print $1}'`
-echo "$IP `hostname -f`" >> /etc/hosts
-oc cluster up --public-hostname $IP.xip.io --routing-suffix $IP.xip.io
-docker exec origin oadm policy add-cluster-role-to-user cluster-admin admin --config=/var/lib/origin/openshift.local.config/master/admin.kubeconfig
-docker exec origin oadm policy add-cluster-role-to-user cluster-admin developer --config=/var/lib/origin/openshift.local.config/master/admin.kubeconfig
+VERSION="v0.1.0"
+yum -y install xorg-x11-xauth remote-viewer
+sed -i "s/SELINUX=enforcing/SELINUX=permissive/" /etc/selinux/config
+setenforce 0
+oc project kube-system
+wget https://github.com/kubevirt/kubevirt/releases/download/$VERSION/kubevirt.yaml
+wget https://github.com/kubevirt/kubevirt/releases/download/$VERSION/spice-proxy.yaml
+oc adm policy add-scc-to-user privileged -z kubevirt-infra
+oc adm policy add-scc-to-user hostmount-anyuid -z kubevirt-infra
+oc create -f kubevirt.yaml
+oc create -f spice-proxy.yaml
+oc expose deploy haproxy  --port=8184
+oc expose svc haproxy
+wget https://github.com/kubevirt/kubevirt/releases/download/$VERSION/virtctl-$VERSION-linux-amd64
+mv virtctl-$VERSION-linux-amd64 /usr/bin/virtctl
+chmod u+x /usr/bin/virtctl
+oc create -f /root/iscsi-demo-target.yaml
