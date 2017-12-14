b7786d435606d4c056736a86975291727e34a91e
karimb <karimboumedhel@gmail.com>
2017-12-14 12:02:19 +0100

    downstream ocp 3.6 and 3.7

--- plans/openshift/subscription.sh
+++ plans/openshift/subscription.sh
@@ -1,1 +1,1 @@
-subscription-manager repos --enable=rhel-7-server-extras-rpms --enable=rhel-7-server-ose-3.6-rpms --enable=rhel-7-fast-datapath-rpms
+subscription37.sh
---
+++ plans/openshift/subscription36.sh
@@ -0,0 +1,1 @@
+subscription-manager repos --enable=rhel-7-server-extras-rpms --enable=rhel-7-server-ose-3.6-rpms --enable=rhel-7-fast-datapath-rpms
