110fc1a59a9f1b5867bb97806c32755464efaaf3
karimb <karimboumedhel@gmail.com>
2017-12-19 13:03:05 +0100

    turned builder plan into dynamic

--- plans/builder/copr.sh
+++ plans/builder/copr.sh
@@ -11,14 +11,16 @@ mkdir -p /root/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
 mv /root/$SHORT /root/rpmbuild/SOURCES
 rpmbuild -bs /root/package.spec
 copr-cli build kcli /root/rpmbuild/SRPMS/kcli-$VERSION-$RELEASE.src.rpm
-BUILD=00`copr-cli get-package --with-latest-build --name kcli karmab/kcli | jq -r '.latest_build.id'`
+BUILD=00`copr-cli get-package --with-latest-build --name [[ package ]] [[ user ]]/[[ package ]] | jq -r '.latest_build.id'`
+[% if debian %]
 gem install package_cloud
-wget -P /root https://copr-be.cloud.fedoraproject.org/results/karmab/kcli/fedora-25-x86_64/$BUILD-kcli/kcli-$VERSION-$RELEASE.x86_64.rpm
+wget -P /root https://copr-be.cloud.fedoraproject.org/results/[[ user ]]/[[ package ]]/fedora-25-x86_64/$BUILD-[[ package]]/[[ package ]]-$VERSION-$RELEASE.x86_64.rpm
 cd /root
 alien -d *rpm
-/usr/local/bin/package_cloud push karmab/kcli/ubuntu/zesty /root/*deb
-/usr/local/bin/package_cloud push karmab/kcli/ubuntu/xenial /root/*deb
-/usr/local/bin/package_cloud push karmab/kcli/ubuntu/yakkety /root/*deb
-/usr/local/bin/package_cloud push karmab/kcli/debian/jessie /root/*deb
-/usr/local/bin/package_cloud push karmab/kcli/debian/stretch /root/*deb
+/usr/local/bin/package_cloud push [[ user ]]/[[ package ]]/ubuntu/zesty /root/*deb
+/usr/local/bin/package_cloud push [[ user ]]/[[ package ]]/ubuntu/xenial /root/*deb
+/usr/local/bin/package_cloud push [[ user ]]/[[ package ]]/ubuntu/yakkety /root/*deb
+/usr/local/bin/package_cloud push [[ user ]]/[[ package ]]/debian/jessie /root/*deb
+/usr/local/bin/package_cloud push [[ user ]]/[[ package ]]/debian/stretch /root/*deb
+[% endif %]
 poweroff
