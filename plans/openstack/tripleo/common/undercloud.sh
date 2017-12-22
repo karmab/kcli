b42a4133e940058a1b07270d3fa4a049b8372769
Manuel Valle Diez <mvalle@mvalledi.laptop.redhat.local>
2017-12-22 13:32:57 +0100

    Refactor tripleO plans

--- plans/openstack/tripleo/common/undercloud.sh
+++ plans/openstack/tripleo/common/undercloud.sh
@@ -1,6 +1,5 @@
-VERSION="11"
-#TYPE=""
-TYPE="advanced"
+VERSION="10"
+
 ssh-keyscan -H 192.168.101.1 >> ~/.ssh/known_hosts
 ssh-keygen -N '' -t rsa -f /home/stack/.ssh/id_rsa
 ssh-copy-id -i ~/.ssh/id_rsa.pub root@192.168.101.1
@@ -11,14 +10,13 @@ ssh-copy-id -i ~/.ssh/id_rsa.pub root@192.168.101.1
 ##mv /root/openvswitch-2.5.0-22.git20160727.el7.x86_64.rpm /var/www/html/o.rpm
 ##restorecon -Frv /var/www/html
 mkdir images
-wget https://raw.githubusercontent.com/karmab/kcli/master/plans/openstack/tripleo/templates.tar.gz
 tar zxvf templates.tar.gz
 cd ~/images
 for i in /usr/share/rhosp-director-images/overcloud-full-latest-$VERSION.0.tar /usr/share/rhosp-director-images/ironic-python-agent-latest-$VERSION.0.tar; do tar -xvf $i; done
 source ~/stackrc
 openstack overcloud image upload --image-path /home/stack/images
 neutron subnet-update `neutron subnet-list -c id -f value` --dns-nameserver 8.8.8.8
-cd ~/$TYPE
+
 sh instackenv.sh
 openstack baremetal import --json ~/instackenv.json
 sleep 3
