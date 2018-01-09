4f20f264d392881e36791ccfda618032d4d6731c
karimb <karimboumedhel@gmail.com>
2018-01-09 16:39:20 +0100

    refactor upstream openshift plan to make it modular

--- plans/openshift/requirements37.sh
+++ plans/openshift/requirements37.sh
@@ -6,13 +6,14 @@
                   '3.6' : 'v3.6.1/openshift-origin-client-tools-v3.6.1-008f2d5',
                   '3.7' : 'v3.7.0/openshift-origin-client-tools-v3.7.0-7ed6862',
                }
+set releaseurl = releaseurls[version]
 -%]
 yum -y install wget docker git
 systemctl enable docker
 sed -i "s@# INSECURE_REGISTRY=.*@INSECURE_REGISTRY='--insecure-registry 172.30.0.0/16'@" /etc/sysconfig/docker
 echo -e "DEVS=/dev/vdb\nVG=dockervg" > /etc/sysconfig/docker-storage-setup
 docker-storage-setup
-wget -O /root/oc.tar.gz https://github.com/openshift/origin/releases/download/[[ releaseurls[version] ]]-linux-64bit.tar.gz
+wget -O /root/oc.tar.gz https://github.com/openshift/origin/releases/download/[[ releaseurl ]]-linux-64bit.tar.gz
 cd /root ; tar zxvf oc.tar.gz
 mv /root/openshift-origin-client-tools-*/oc /usr/bin
 rm -rf  /root/openshift*
