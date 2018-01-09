772248cd4ede0394f500e7e4208f2209d99faf79
karimb <karimboumedhel@gmail.com>
2018-01-09 11:19:43 +0100

    refactor upstream openshift plan to make it modular

--- plans/openshift/upstream.sh
+++ plans/openshift/upstream.sh
@@ -2,6 +2,7 @@ sed -i '/OPTIONS=.*/c\OPTIONS="--selinux-enabled --insecure-registry 172.30.0.0/
 systemctl start docker --ignore-dependencies
 [% if 'Fedora' in template %]
 sleep 120
+[% endif %]
 export IP=`ip a l  eth0 | grep 'inet ' | cut -d' ' -f6 | awk -F'/' '{ print $1}'`
 oc cluster up --public-hostname $IP.xip.io --routing-suffix $IP.xip.io
 docker exec origin oadm policy add-cluster-role-to-user cluster-admin admin --config=/var/lib/origin/openshift.local.config/master/admin.kubeconfig
