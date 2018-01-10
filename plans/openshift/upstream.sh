3e947a9c918841210c2a42ebfca74d12d021ca75
karimb <karimboumedhel@gmail.com>
2018-01-10 10:58:16 +0100

    allow installing metrics in oc cluster up

--- plans/openshift/upstream.sh
+++ plans/openshift/upstream.sh
@@ -5,9 +5,9 @@ sleep 120
 [% endif %]
 export IP=`ip a l  eth0 | grep 'inet ' | cut -d' ' -f6 | awk -F'/' '{ print $1}'`
 [% if openshift_tag is defined %]
-oc cluster up --public-hostname $IP.xip.io --routing-suffix $IP.xip.io --version=[[ openshift_tag ]]
+oc cluster up --public-hostname $IP.xip.io --routing-suffix $IP.xip.io --version=[[ openshift_tag ]] --metrics=[[ metrics ]]
 [% else %]
-oc cluster up --public-hostname $IP.xip.io --routing-suffix $IP.xip.io
+oc cluster up --public-hostname $IP.xip.io --routing-suffix $IP.xip.io --metrics=[[ metrics ]]
 [% endif %]
 #docker exec origin oadm policy add-cluster-role-to-user cluster-admin admin --config=/var/lib/origin/openshift.local.config/master/admin.kubeconfig
 #docker exec origin oadm policy add-cluster-role-to-user cluster-admin developer --config=/var/lib/origin/openshift.local.config/master/admin.kubeconfig
