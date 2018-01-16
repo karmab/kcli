70ec2f4938dd20afe743510fcd9b39e53150efcc
karimb <karimboumedhel@gmail.com>
2018-01-16 15:38:39 +0100

    k8s plan fixes

--- plans/kubernetes/master.sh
+++ plans/kubernetes/master.sh
@@ -6,8 +6,9 @@ echo "export KUBECONFIG=/root/admin.conf" >>/root/.bashrc
 kubectl taint nodes --all node-role.kubernetes.io/master-
 kubectl create -f /root/kube-flannel-rbac.yml
 kubectl apply -f /root/kube-flannel.yml
-export TOKEN=`kubeadm token list  | tail -1 | cut -f1 -d' '`
-export CMD="kubeadm join --token $TOKEN kumaster:6443"
+#export TOKEN=`kubeadm token list  | tail -1 | cut -f1 -d' '`
+#export CMD="kubeadm join --token $TOKEN kumaster:6443"
+export CMD="kubeadm token create --print-join-command"
 echo $CMD > /root/join.sh
 sleep 80
 [% for number in range(1,nodes+1) %]
