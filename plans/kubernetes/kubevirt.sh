fc0b1658da859d3ca69a2c754e875ab2757ceda9
karimb <karimboumedhel@gmail.com>
2018-01-17 00:23:52 +0100

    additional kubevirt artefacts

--- plans/kubernetes/kubevirt.sh
+++ plans/kubernetes/kubevirt.sh
@@ -11,4 +11,5 @@ wget https://github.com/kubevirt/kubevirt/releases/download/$VERSION/virtctl-$VE
 mv virtctl-$VERSION-linux-amd64 /usr/bin/virtctl
 chmod u+x /usr/bin/virtctl
 kubectl create -f /root/iscsi-demo-target.yaml
+kubectl patch deployment/iscsi-demo-target-tgtd --patch '{"spec":{"template":{"spec":{"nodeSelector":{"kubernetes.io/hostname":"kumaster"}}}}}'
 kubectl config set-context `kubectl config current-context` --namespace=default
