6a8d79027274a1ed89e7f90cb479c1cda5fc344f
karimb <karimboumedhel@gmail.com>
2018-01-16 23:34:27 +0100

    dont use project with kubectl

--- plans/kubernetes/kubevirt.sh
+++ plans/kubernetes/kubevirt.sh
@@ -2,7 +2,7 @@ VERSION="v[[ kubevirt_version ]]"
 yum -y install xorg-x11-xauth remote-viewer wget
 sed -i "s/SELINUX=enforcing/SELINUX=permissive/" /etc/selinux/config
 setenforce 0
-kubectl project kube-system
+kubectl config set-context `kubectl config current-context` --namespace=kube-system
 wget https://github.com/kubevirt/kubevirt/releases/download/$VERSION/kubevirt.yaml
 kubectl create -f kubevirt.yaml
 kubectl expose deploy haproxy  --port=8184
@@ -11,4 +11,4 @@ wget https://github.com/kubevirt/kubevirt/releases/download/$VERSION/virtctl-$VE
 mv virtctl-$VERSION-linux-amd64 /usr/bin/virtctl
 chmod u+x /usr/bin/virtctl
 kubectl create -f /root/iscsi-demo-target.yaml
-kubectl project default
+kubectl config set-context `kubectl config current-context` --namespace=default
