da001c17c45ef64e91460e4efdd7b480c7d720f2
karimb <karimboumedhel@gmail.com>
2018-02-02 13:50:22 +0100

    bring persistency to openwhisk plan

--- plans/openshift/openwhisk.sh
+++ plans/openshift/openwhisk.sh
@@ -10,3 +10,8 @@ git fetch origin
 git checkout remotes/origin/simplify-deployment-openshift
 oc create -f openshift/ -f kubernetes
 oc volume deployment/couchdb --add --overwrite --claim-name=couchdb-claim --claim-size=100Mi --name=couchdb-data
+oc volume deployment/kafka --add --overwrite --claim-name=kafka-logs-claim --claim-size=100Mi --name=kafka-logs
+oc volume deployment/kafka --add --overwrite --claim-name=kafka-claim --claim-size=100Mi --name=kafka
+oc volume deployment/nginx --add --overwrite --claim-name=nginx-claim --claim-size=100Mi --name=logs
+oc volume deployment/zookeper --add --overwrite --claim-name=zookeper-data-claim --claim-size=100Mi --name=zookeper-data
+oc volume deployment/zookeeper --add --overwrite --claim-name=zookeper-datalog-claim --claim-size=100Mi --name=zookeeper-datalog
