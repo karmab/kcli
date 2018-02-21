86ccc67a8a6f32558f97f9c36e8c0b02a41cda8e
karimb <karimboumedhel@gmail.com>
2018-02-21 23:35:15 +0100

    updated kubevirt plan for k8s

--- plans/kubernetes/kubevirt.sh
+++ plans/kubernetes/kubevirt.sh
@@ -1,15 +1,11 @@
-VERSION="v[[ kubevirt_version ]]"
-yum -y install xorg-x11-xauth remote-viewer wget
+VERSION="[[ kubevirt_version ]]"
+yum -y install xorg-x11-xauth virt-viewer wget
 sed -i "s/SELINUX=enforcing/SELINUX=permissive/" /etc/selinux/config
 setenforce 0
 kubectl config set-context `kubectl config current-context` --namespace=kube-system
 wget https://github.com/kubevirt/kubevirt/releases/download/$VERSION/kubevirt.yaml
 kubectl create -f kubevirt.yaml
-kubectl expose deploy haproxy  --port=8184
-kubectl expose svc haproxy
 wget https://github.com/kubevirt/kubevirt/releases/download/$VERSION/virtctl-$VERSION-linux-amd64
 mv virtctl-$VERSION-linux-amd64 /usr/bin/virtctl
 chmod u+x /usr/bin/virtctl
-kubectl create -f /root/iscsi-demo-target.yaml
-kubectl patch deployment/iscsi-demo-target-tgtd --patch '{"spec":{"template":{"spec":{"nodeSelector":{"kubernetes.io/hostname":"kumaster"}}}}}'
 kubectl config set-context `kubectl config current-context` --namespace=default
