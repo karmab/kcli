1cd067e752a6532bb3f956f870ee0662a437ed74
karmab <karimboumedhel@gmail.com>
2018-03-08 20:10:54 +0100

    gluster with heketi plan

---
+++ plans/gluster/cns/heketi.sh
@@ -0,0 +1,17 @@
+yum -y install heketi-client heketi bind-utils
+rm -f /etc/heketi/heketi.json
+cp /root/heketi.json /etc/heketi
+chown heketi.heketi /etc/heketi/heketi.json
+sed -i "s/ -config/ --config/" /usr/lib/systemd/system/heketi.service
+cp /root/.ssh/id_rsa /var/lib/heketi/
+chown heketi.heketi /var/lib/heketi/id_rsa
+systemctl enable heketi
+systemctl start heketi
+IP1=`dig +short gluster01`
+IP2=`dig +short gluster02`
+IP3=`dig +short gluster03`
+sed -i "s/gluster01/$IP1/" /root/topology.json
+sed -i "s/gluster02/$IP2/" /root/topology.json
+sed -i "s/gluster03/$IP3/" /root/topology.json
+export HEKETI_CLI_SERVER=http://127.0.0.1:8080
+heketi-cli topology load --json=/root/topology.json
---
+++ plans/gluster/cns/requisites.sh
@@ -0,0 +1,11 @@
+yum -y install glusterfs-server glusterfs-ganesha glusterfs glusterfs-geo-replication glusterfs-cli samba
+systemctl start glusterd
+systemctl enable glusterd
+systemctl start smb
+systemctl enable smb
+systemctl start nmb
+systemctl enable nmb
+#tuned-adm profile rhgs-random-io
+tuned-adm profile throughput-performance
+pvcreate /dev/vdb
+pvcreate /dev/vdc
