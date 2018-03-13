7afa1bc33550a835a6989e44afc92bbeda665d75
karmab <karimboumedhel@gmail.com>
2018-03-13 12:21:06 +0100

    optionally enable metrics

--- plans/openshift/downstream.sh
+++ plans/openshift/downstream.sh
@@ -5,6 +5,10 @@ systemctl enable rhel-push-plugin
 systemctl start rhel-push-plugin
 systemctl start docker --ignore-dependencies
 export IP=`ip a l  eth0 | grep 'inet ' | cut -d' ' -f6 | awk -F'/' '{ print $1}'`
+[% if metrics %]
 oc cluster up --public-hostname $IP.xip.io --routing-suffix $IP.xip.io --image=registry.access.redhat.com/openshift3/ose --metrics
+[% else %]
+oc cluster up --public-hostname $IP.xip.io --routing-suffix $IP.xip.io --image=registry.access.redhat.com/openshift3/ose
+[% endif %]
 oadm policy add-cluster-role-to-user cluster-admin admin --config=/var/lib/origin/openshift.local.config/master/admin.kubeconfig
 oadm policy add-cluster-role-to-user cluster-admin developer --config=/var/lib/origin/openshift.local.config/master/admin.kubeconfig
