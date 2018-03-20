829286e68a1177bd61813ee7e095e887a0c169b9
karmab <karimboumedhel@gmail.com>
2018-03-20 21:13:20 +0100

    temporarily default to q35

--- plans/kubernetes/pre.sh
+++ plans/kubernetes/pre.sh
@@ -2,6 +2,7 @@ echo net.bridge.bridge-nf-call-iptables=1 >> /etc/sysctl.d/99-sysctl.conf
 sysctl -p
 setenforce 0
 sed -i "s/SELINUX=enforcing/SELINUX=permissive/" /etc/selinux/config
+sed -i "s/--selinux-enabled //" /etc/sysconfig/docker
 yum install -y docker kubelet kubeadm kubectl kubernetes-cni
 systemctl enable docker && systemctl start docker
 systemctl enable kubelet && systemctl start kubelet
