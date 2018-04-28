0b26c866a3c942cac7ae535396ebedab25b7791d
karmab <karimboumedhel@gmail.com>
2018-04-28 08:20:18 +0200

    tiny fixes for foreman plan

--- plans/foreman/upstream.sh
+++ plans/foreman/upstream.sh
@@ -1,5 +1,5 @@
-rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
+rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 yum -y install epel-release https://yum.theforeman.org/releases/[[ version ]]/el7/x86_64/foreman-release.rpm
 yum -y install foreman-installer wget
 echo `hostname -I` `hostname -s`.default `hostname -s` >> /etc/hosts
-foreman-installer --foreman-admin-username [[ user ]] --foreman-admin-password [[ password ]] --foreman-initial-location [[ location ]]--foreman-initial-organization [[ organization ]]
+foreman-installer --foreman-admin-username [[ user ]] --foreman-admin-password [[ password ]] --foreman-initial-location [[ location ]] --foreman-initial-organization [[ organization ]]
