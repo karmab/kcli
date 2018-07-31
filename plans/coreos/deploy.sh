8f7f9f91fb3955e3faa1952aafdc79b5b396029d
karmab <karimboumedhel@gmail.com>
2018-07-31 18:10:42 +0200

    tweaks on the kubernetes coreos plan

--- plans/coreos/deploy.sh
+++ plans/coreos/deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-IP=`ifconfig %s | grep \"inet \" | awk '{print $2}'`
+IP=`ifconfig eth0 | grep "inet " | awk '{print $2}'`
 ETCD_ENDPOINT="http://$IP:2379"
 mkdir -p /etc/kubernetes/cni/net.d
 echo -e "DOCKER_OPT_BIP=\"\"\nDOCKER_OPT_IPMASQ=\"\"" > /etc/kubernetes/cni/docker_opts_cni.env
 cp /root/10-flannel.conf /etc/kubernetes/cni/net.d
-echo -e FLANNELD_IFACE=$IP\nFLANNELD_ETCD_ENDPOINTS=http://$IP:2379 > /etc/flannel/options.env
+mkdir -p /etc/flannel
+echo -e "FLANNELD_IFACE=$IP\nFLANNELD_ETCD_ENDPOINTS=http://$IP:2379" > /etc/flannel/options.env
 sed -i "s/ADVERTISE_IP/$IP/" /etc/systemd/system/kubelet.service
 sed -i "s@ETCD_ENDPOINT@$ETCD_ENDPOINT@" /root/kube-apiserver.yaml
 sed -i "s/ADVERTISE_IP/$IP/" /root/kube-apiserver.yaml
@@ -26,7 +27,7 @@ export CA_CERT=/etc/kubernetes/ssl/ca.pem
 export ADMIN_KEY=/etc/kubernetes/ssl/admin.pem
 export ADMIN_KEY=/etc/kubernetes/ssl/admin-key.pem
 export ADMIN_CERT=/etc/kubernetes/ssl/admin.pem
-./kubectl config set-cluster default-cluster --server=https://${MASTER_HOST} --certificate-authority=${CA_CERT}
-./kubectl config set-credentials default-admin --certificate-authority=${CA_CERT} --client-key=${ADMIN_KEY} --client-certificate=${ADMIN_CERT}
+./kubectl config set-cluster default-cluster --server=https://$IP --certificate-authority=$CA_CERT
+./kubectl config set-credentials default-admin --certificate-authority=$CA_CERT --client-key=$ADMIN_KEY --client-certificate=$ADMIN_CERT
 ./kubectl config set-context default-system --cluster=default-cluster --user=default-admin
 ./kubectl config use-context default-system
--- plans/coreos/openssl.sh
+++ plans/coreos/openssl.sh
@@ -4,7 +4,7 @@ cd /etc/kubernetes/ssl
 openssl genrsa -out ca-key.pem 2048
 openssl req -x509 -new -nodes -key ca-key.pem -days 10000 -out ca.pem -subj "/CN=kube-ca"
 openssl genrsa -out apiserver-key.pem 2048
-MASTER_HOST=`ifconfig %s | grep \"inet \" | awk '{print $2}'`
+MASTER_HOST=`ifconfig eth0 | grep "inet " | awk '{print $2}'`
 sed -i "s/MASTER_HOST/$MASTER_HOST/" /root/openssl.cnf
 openssl req -new -key apiserver-key.pem -out apiserver.csr -subj "/CN=kube-apiserver" -config /root/openssl.cnf
 openssl x509 -req -in apiserver.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out apiserver.pem -days 365 -extensions v3_req -extfile /root/openssl.cnf
