59e5d385047fc0fe63c94ae4cbb05d4e969ab051
karmab <karimboumedhel@gmail.com>
2018-07-31 19:13:16 +0200

    kubevirt on top of single kubernetes

---
+++ plans/kubernetes/single/kubevirt.sh
@@ -0,0 +1,13 @@
+VERSION="[[ kubevirt_version ]]"
+yum -y install xorg-x11-xauth virt-viewer wget
+sed -i "s/SELINUX=enforcing/SELINUX=permissive/" /etc/selinux/config
+setenforce 0
+kubectl config set-context `kubectl config current-context` --namespace=kube-system
+wget https://github.com/kubevirt/kubevirt/releases/download/$VERSION/kubevirt.yaml
+kubectl create -f kubevirt.yaml --validate=false
+wget https://github.com/kubevirt/kubevirt/releases/download/$VERSION/virtctl-$VERSION-linux-amd64
+mv virtctl-$VERSION-linux-amd64 /usr/bin/virtctl
+chmod u+x /usr/bin/virtctl
+kubectl config set-context `kubectl config current-context` --namespace=default
+docker pull karmab/kcli
+echo alias kcli=\'docker run --security-opt label:disable -it --rm -v ~/.kube:/root/.kube -v ~/.ssh:/root/.ssh -v ~/.kcli:/root/.kcli karmab/kcli\' >> /root/.bashrc
