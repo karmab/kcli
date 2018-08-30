84bf6cfd020d3db8900355c8bfb8a7ffb591ab4b
karmab <karimboumedhel@gmail.com>
2018-08-30 13:14:45 +0200

    fix helm plan

--- plans/openshift/helm.sh
+++ plans/openshift/helm.sh
@@ -2,7 +2,7 @@ VERSION=v[[ helm_version ]]
 oc login -u system:admin
 oc new-project helm
 oc create serviceaccount helm --namespace=helm
-docker exec origin oadm policy add-cluster-role-to-user cluster-admin system:serviceaccount:helm:helm
+oc adm policy add-cluster-role-to-user cluster-admin system:serviceaccount:helm:helm
 curl -LO https://kubernetes-helm.storage.googleapis.com/helm-$VERSION-linux-amd64.tar.gz
 tar zxvf helm-$VERSION-linux-amd64.tar.gz
 mv linux-amd64/helm /usr/bin
