59736e0ebc1b8a0063a8163b2a2b4299bc5fe353
karmab <karimboumedhel@gmail.com>
2018-09-07 17:11:43 +0200

    provide minishift plan

--- plans/openshift/minishift.sh
+++ plans/openshift/minishift.sh
@@ -1,10 +1,21 @@
 export MINISHIFT_VERSION="[[ minishift_version ]]"
 export OPENSHIFT_VERSION="v[[ openshift_version ]]"
+[% if driver == 'generic' %]
+yum -y install docker
+systemctl enable docker
+systemctl start docker
+[% else %]
+curl -L https://github.com/dhiltgen/docker-machine-kvm/releases/download/v0.7.0/docker-machine-driver-kvm -o /usr/local/bin/docker-machine-driver-kvm
+chmod +x /usr/local/bin/docker-machine-driver-kvm
+yum -y install libvirt qemu
+systemctl enable libvirtd
+systemctl start libvirtd
+[% endif %]
 curl -L https://github.com/minishift/minishift/releases/download/v$MINISHIFT_VERSION/minishift-$MINISHIFT_VERSION-linux-amd64.tgz > /root/minishift-$MINISHIFT_VERSION-linux-amd64.tgz
 tar zxvf /root/minishift-$MINISHIFT_VERSION-linux-amd64.tgz
 mv minishift-$MINISHIFT_VERSION-linux-amd64/minishift /usr/bin/
 chmod u+x /usr/bin/minishift
-minishift start --vm-driver=none --iso-url centos
+minishift start --vm-driver=[[ driver ]] --iso-url centos
 #eval $(minishift oc-env)
 #oc login -u system:admin
 #oc adm policy add-cluster-role-to-user cluster-admin admin
