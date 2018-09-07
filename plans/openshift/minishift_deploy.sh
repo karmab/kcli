98be69ed1740576ef6ee57d1913a6593a70f6050
karmab <karimboumedhel@gmail.com>
2018-09-07 22:51:55 +0200

    also support generic install for minishift

--- plans/openshift/minishift_deploy.sh
+++ plans/openshift/minishift_deploy.sh
@@ -1,4 +1,9 @@
+[% if driver == 'generic' %] 
+ssh-keyscan -H 127.0.0.1 >> ~/.ssh/known_hosts
+minishift start --vm-driver=generic --iso-url centos --remote-ipaddress 127.0.0.1 --remote-ssh-user root --remote-ssh-key ~/.ssh/id_rsa
+[% else %]]
 minishift start --vm-driver=kvm --iso-url centos
+[% endif %]
 eval $(minishift oc-env)
 oc login -u system:admin
 oc adm policy add-cluster-role-to-user cluster-admin admin
