07da4135ecd5903579360321bed63c7f2ac01912
karmab <karimboumedhel@gmail.com>
2018-09-11 10:34:57 +0200

    minishift improvements

--- plans/openshift/minishift_deploy.sh
+++ plans/openshift/minishift_deploy.sh
@@ -1,10 +1,11 @@
 [% if driver == 'generic' %] 
 ssh-keyscan -H 127.0.0.1 >> ~/.ssh/known_hosts
-minishift start --vm-driver=generic --iso-url centos --remote-ipaddress 127.0.0.1 --remote-ssh-user root --remote-ssh-key ~/.ssh/id_rsa
-[% else %]]
-minishift start --vm-driver=kvm --iso-url centos
+minishift start --vm-driver=generic --iso-url centos --remote-ipaddress 127.0.0.1 --remote-ssh-user root --remote-ssh-key ~/.ssh/id_rsa --memory [[ minishift_memory ]]
+[% else %]
+minishift start --vm-driver=kvm --iso-url centos --memory [[ minishift_memory ]]
 [% endif %]
 eval $(minishift oc-env)
+minishift oc-env >>/root/.bashrc
 oc login -u system:admin
 oc adm policy add-cluster-role-to-user cluster-admin admin
 oc adm policy add-cluster-role-to-user cluster-admin developer
