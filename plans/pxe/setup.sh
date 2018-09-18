81c1c5c8de25bede1c7d494c03d01b27cd408020
karmab <karimboumedhel@gmail.com>
2018-09-18 10:04:51 +0200

    make the distro a little bit more generic for pxe

--- plans/pxe/setup.sh
+++ plans/pxe/setup.sh
@@ -1,14 +1,14 @@
 yum -y install httpd xinetd syslinux tftp-server wget
-wget http://repo.nixval.com/CentOS/7/isos/x86_64/CentOS-7-x86_64-Minimal-1804.iso
-mount -o loop CentOS-7-x86_64-Minimal-1804.iso /mnt/
+wget -O distro.iso [[ url ]]
+mount -o loop distro.iso /mnt/
 cp -a /mnt/* /var/www/html
 cp /root/ks.cfg /var/www/html
 chmod -R 755 /var/www/html
 setenforce 0
 cp -a /usr/share/syslinux/* /var/lib/tftpboot/
-mkdir /var/lib/tftpboot/centos7
-cp /mnt/images/pxeboot/vmlinuz  /var/lib/tftpboot/centos7
-cp /mnt/images/pxeboot/initrd.img  /var/lib/tftpboot/centos7
+mkdir /var/lib/tftpboot/distro
+cp /mnt/images/pxeboot/vmlinuz  /var/lib/tftpboot/distro
+cp /mnt/images/pxeboot/initrd.img  /var/lib/tftpboot/distro
 mkdir /var/lib/tftpboot/pxelinux.cfg
 cp /root/pxeserver_default /var/lib/tftpboot/pxelinux.cfg/default
 chmod 777 /var/lib/tftpboot/pxelinux.cfg/default
@@ -17,4 +17,3 @@ systemctl restart xinetd
 systemctl restart httpd
 systemctl enable xinetd
 systemctl enable httpd
-
