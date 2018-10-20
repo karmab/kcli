03f0bed279c7dc2682d5e11438f385f3a82c6113
karmab <karimboumedhel@gmail.com>
2018-10-20 13:47:05 +0200

    set default domain in ocp multi plan

--- plans/openshift/multi/install.sh
+++ plans/openshift/multi/install.sh
@@ -2,10 +2,16 @@
 yum -y install openshift-ansible screen bind-utils
 sed -i "s/#host_key_checking/host_key_checking = True/" /etc/ansible/ansible.cfg
 sed -i "s/#log_path/log_path/" /etc/ansible/ansible.cfg
-#export IP=`dig +short master01.[[ domain ]]`
-#sed -i "s/openshift_master_default_subdomain=.*/openshift_master_default_subdomain=$IP.xip.io/" /root/hosts
-#sed -i "s/openshift_master_cluster_hostname=.*/openshift_master_cluster_hostname=$IP.xip.io/" /root/hosts
-#sed -i "s/openshift_master_cluster_public_hostname=.*/openshift_master_cluster_public_hostname=$IP.xip.io/" /root/hosts
+export MASTERIP=`dig +short master01.[[ domain ]]`
+[% if infras > 0 -%]
+export ROUTERIP=`dig +short infra01.[[ domain ]]`
+[% else %]
+export ROUTERIP=$MASTERIP
+[% endif %]
+sed -i "s/#openshift_master_default_subdomain=.*/openshift_master_default_subdomain=app.$ROUTERIP.xip.io/" /root/inventory
+sed -i "s/#openshift_master_cluster_hostname=.*/openshift_master_cluster_hostname=$MASTERIP.xip.io/" /root/inventory
+sed -i "s/#openshift_master_cluster_public_hostname=.*/openshift_master_cluster_public_hostname=$MASTERIP.xip.io/" /root/inventory
+
 [% if type == 'ovirt' %]
 sh /root/fix_inventory.sh
 [% endif %]
