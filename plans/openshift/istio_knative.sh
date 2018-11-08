f9fe1f46b0211b3bd2709f2706c988ddf3817b7e
karmab <karimboumedhel@gmail.com>
2018-11-08 12:10:43 +0100

    add support for knative and istio in upstream.yml openshift plan

---
+++ plans/openshift/istio_knative.sh
@@ -0,0 +1,3 @@
+oc new-project istio-operator
+oc new-app -f https://raw.githubusercontent.com/Maistra/openshift-ansible/maistra-0.2.0-ocp-3.1.0-istio-1.0.2/istio/istio_community_operator_template.yaml --param=OPENSHIFT_ISTIO_MASTER_PUBLIC_URL=https://$DNS:8443
+oc create -f /root/istio_knative.yml
