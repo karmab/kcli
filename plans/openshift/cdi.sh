225c235a9ec6b14df1961a5e3f21b296e3e5a412
karmab <karimboumedhel@gmail.com>
2018-11-14 13:03:12 +0100

    fix kubevirt ui plan

--- plans/openshift/cdi.sh
+++ plans/openshift/cdi.sh
@@ -1,8 +1,5 @@
-wget -P /root/ https://github.com/kubevirt/containerized-data-importer/releases/download/[[ cdi_version ]]/cdi-controller.yaml
-wget -P /root https://github.com/kubevirt/containerized-data-importer/releases/download/[[ cdi_version ]]/golden-pvc.yaml
-wget -P /root https://github.com/kubevirt/containerized-data-importer/releases/download/[[ cdi_version ]]/endpoint-secret.yaml
 oc new-project golden
-sed -i "s/namespace:.*/namespace: golden/" cdi-controller.yaml
 oc adm policy add-scc-to-user privileged system:serviceaccount:golden:default
 oc adm policy add-cluster-role-to-user cluster-admin system:serviceaccount:golden:cdi-apiserver
 oc create -f /root/cdi-controller.yaml
+oc expose svc cdi-uploadproxy -n golden
--- plans/openshift/multus.sh
+++ plans/openshift/multus.sh
@@ -1,4 +1,9 @@
 oc apply -f /root/multus.yml
 oc apply -f /root/cni-plugins.yml
 oc apply -f /root/ovs-cni.yml
-oc apply -f /root/ovs-vsctl.yml
+yum -y install openvswitch
+systemctl start openvswitch
+systemctl enable openvswitch
+ovs-vsctl add-br br1
+ovs-vsctl add-port br1 eth1
+oc apply -f /root/nad_br1.yml
