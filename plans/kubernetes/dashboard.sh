d834ee283236525f1664cdb23088d64b2be80ef8
karmab <karimboumedhel@gmail.com>
2018-11-15 11:05:34 +0100

    grant privileges to k8s dashboard

--- plans/kubernetes/dashboard.sh
+++ plans/kubernetes/dashboard.sh
@@ -1,2 +1,3 @@
 # kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/alternative/kubernetes-dashboard.yaml
+kubectl create clusterrolebinding kubernetes-dashboard-head --clusterrole=cluster-admin --user=system:serviceaccount:kube-system:kubernetes-dashboard
