baafb27f1fe2925853d4b824b6f8baca3843baf4
karmab <karimboumedhel@gmail.com>
2018-11-26 19:20:42 +0100

    install oc in openshift installer plan

--- plans/openshift/installer_cleanup.sh
+++ plans/openshift/installer_cleanup.sh
@@ -1,9 +1,9 @@
 URI="qemu+ssh://root@192.168.122.1/system"
-virsh -c ${URI} destroy master-0
-virsh -c ${URI} undefine master-0
-virsh -c ${URI} destroy bootstrap
-virsh -c ${URI} undefine bootstrap
-ssh root@192.168.122.1 rm -rf /var/lib/libvirt/images/bootstrap* /var/lib/libvirt/images/coreos_base /var/lib/libvirt/images/master* /var/lib/libvirt/images/worker.ign master-0.ign
+virsh -c ${URI} destroy testk-master-0
+virsh -c ${URI} undefine testk-master-0
+virsh -c ${URI} destroy testk-bootstrap
+virsh -c ${URI} undefine testk-bootstrap
+ssh root@192.168.122.1 rm -rf /var/lib/libvirt/images/testk-* /var/lib/libvirt/images/coreos_base
 virsh -c ${URI} pool-refresh default
 virsh -c ${URI} net-destroy [[ cluster ]]
 virsh -c ${URI} net-undefine [[ cluster ]]
