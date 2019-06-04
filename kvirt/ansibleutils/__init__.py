87245c224a21b8f5ea6a9210e20ba6eec1d8a066
Johan Belin <johan.belin@safespring.com>
2019-06-04 17:05:06 +0200

    kvm: fix adding tunnel info during inventory generation

--- kvirt/ansibleutils/__init__.py
+++ kvirt/ansibleutils/__init__.py
@@ -123,7 +123,6 @@ def make_plan_inventory(vms_to_host, plan, vms, groups={}, user=None, yamlinvent
     :param yamlinventory:
     """
     inventory = {}
-    clientinventory = {}
     inventoryfile = "/tmp/%s.inv.yaml" % plan if yamlinventory else "/tmp/%s.inv" % plan
     pprint("Generating inventory %s" % inventoryfile, color='blue')
     if groups:
@@ -136,45 +135,31 @@ def make_plan_inventory(vms_to_host, plan, vms, groups={}, user=None, yamlinvent
             for name in nodes:
                 k = vms_to_host[name].k
                 client = vms_to_host[name].client
-                if client not in clientinventory:
-                    clientinventory[client] = {'hosts': {}}
                 inv = vm_inventory(k, name, user=user, yamlinventory=yamlinventory)
                 if inv is not None:
-                    clientinventory[client]['hosts'][name] = inv
                     inventory[plan]['children'][group]['hosts'][name] = inv
+                if vms_to_host[name].tunnel:
+                    tunnelinfo = "-o ProxyCommand=\"ssh -p %s -W %%h:%%p %s@%s\"" % (vms_to_host[name].port, vms_to_host[name].user, vms_to_host[name].host)
+                    if yamlinventory:
+                        inventory[plan]['children'][group]['hosts'][name]['ansible_ssh_common_args'] = tunnelinfo
+                    else:
+                        inventory[plan]['hosts'][name] += " ansible_ssh_common_args='%s'" % tunnelinfo
     else:
         inventory[plan] = {'hosts': {}}
         for name in vms:
             k = vms_to_host[name].k
             client = vms_to_host[name].client
-            if client not in clientinventory:
-                clientinventory[client] = {'hosts': {}}
             inv = vm_inventory(k, name, user=user, yamlinventory=yamlinventory)
             if inv is not None:
                 inventory[plan]['hosts'][name] = inv
-                clientinventory[client]['hosts'][name] = inv
-    for entry in vms_to_host.values():
-        client = entry.client
-        tunnel = entry.tunnel
-        tunneluser = entry.user
-        tunnelport = entry.port
-        tunnelhost = entry.host
-        if tunnel:
-            tunnelinfo = "-o ProxyCommand=\"ssh -p %s -W %%h:%%p %s@%s\"" % (tunnelport, tunneluser, tunnelhost)
-            if client in clientinventory and 'vars' not in clientinventory[client]:
-                clientinventory[client]['vars'] = {'ansible_ssh_common_args': tunnelinfo}
-    for name in inventory[plan]['hosts']:
-        for client in clientinventory:
-            if name in clientinventory[client]['hosts']:
-                tunnelinfo = clientinventory[client]['vars']['ansible_ssh_common_args']
+            if vms_to_host[name].tunnel:
+                tunnelinfo = "-o ProxyCommand=\"ssh -p %s -W %%h:%%p %s@%s\"" % (vms_to_host[name].port, vms_to_host[name].user, vms_to_host[name].host)
                 if yamlinventory:
                     inventory[plan]['hosts'][name]['ansible_ssh_common_args'] = tunnelinfo
                 else:
                     inventory[plan]['hosts'][name] += " ansible_ssh_common_args='%s'" % tunnelinfo
-                break
     with open(inventoryfile, "w") as f:
         if yamlinventory:
-            # inventory.update(clientinventory)
             dump({'all': {'children': inventory}}, f, default_flow_style=False)
         else:
             inventorystr = ''
