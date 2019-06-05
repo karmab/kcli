66ebdf1dd2dbd8d2926e852b3e3c0b077d7efc4a
Johan Belin <johan.belin@safespring.com>
2019-06-05 14:34:55 +0200

    Generate inventory with both grouped and ungouped hosts

--- kvirt/ansibleutils/__init__.py
+++ kvirt/ansibleutils/__init__.py
@@ -125,6 +125,7 @@ def make_plan_inventory(vms_to_host, plan, vms, groups={}, user=None, yamlinvent
     inventory = {}
     inventoryfile = "/tmp/%s.inv.yaml" % plan if yamlinventory else "/tmp/%s.inv" % plan
     pprint("Generating inventory %s" % inventoryfile, color='blue')
+    all = vms
     if groups:
         inventory[plan] = {'children': {}}
         for group in groups:
@@ -133,31 +134,36 @@ def make_plan_inventory(vms_to_host, plan, vms, groups={}, user=None, yamlinvent
             nodes = groups[group]
             inventory[plan]['children'][group]['hosts'] = {}
             for name in nodes:
+                all.remove(name)
                 k = vms_to_host[name].k
-                client = vms_to_host[name].client
                 inv = vm_inventory(k, name, user=user, yamlinventory=yamlinventory)
                 if inv is not None:
                     inventory[plan]['children'][group]['hosts'][name] = inv
                 if vms_to_host[name].tunnel:
-                    tunnelinfo = "-o ProxyCommand=\"ssh -p %s -W %%h:%%p %s@%s\"" % (vms_to_host[name].port, vms_to_host[name].user, vms_to_host[name].host)
+                    tunnelinfo = "-o ProxyCommand=\"ssh -p %s -W %%h:%%p %s@%s\"" % (
+                        vms_to_host[name].port,
+                        vms_to_host[name].user,
+                        vms_to_host[name].host)
                     if yamlinventory:
                         inventory[plan]['children'][group]['hosts'][name]['ansible_ssh_common_args'] = tunnelinfo
                     else:
                         inventory[plan]['hosts'][name] += " ansible_ssh_common_args='%s'" % tunnelinfo
-    else:
-        inventory[plan] = {'hosts': {}}
-        for name in vms:
-            k = vms_to_host[name].k
-            client = vms_to_host[name].client
-            inv = vm_inventory(k, name, user=user, yamlinventory=yamlinventory)
-            if inv is not None:
-                inventory[plan]['hosts'][name] = inv
-            if vms_to_host[name].tunnel:
-                tunnelinfo = "-o ProxyCommand=\"ssh -p %s -W %%h:%%p %s@%s\"" % (vms_to_host[name].port, vms_to_host[name].user, vms_to_host[name].host)
-                if yamlinventory:
-                    inventory[plan]['hosts'][name]['ansible_ssh_common_args'] = tunnelinfo
-                else:
-                    inventory[plan]['hosts'][name] += " ansible_ssh_common_args='%s'" % tunnelinfo
+
+    inventory[plan]['hosts'] = {}
+    for name in all:
+        k = vms_to_host[name].k
+        inv = vm_inventory(k, name, user=user, yamlinventory=yamlinventory)
+        if inv is not None:
+            inventory[plan]['hosts'][name] = inv
+        if vms_to_host[name].tunnel:
+            tunnelinfo = "-o ProxyCommand=\"ssh -p %s -W %%h:%%p %s@%s\"" % (
+                vms_to_host[name].port,
+                vms_to_host[name].user,
+                vms_to_host[name].host)
+            if yamlinventory:
+                inventory[plan]['hosts'][name]['ansible_ssh_common_args'] = tunnelinfo
+            else:
+                inventory[plan]['hosts'][name] += " ansible_ssh_common_args='%s'" % tunnelinfo
     with open(inventoryfile, "w") as f:
         if yamlinventory:
             dump({'all': {'children': inventory}}, f, default_flow_style=False)
