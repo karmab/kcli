805c32a46b75a3bc9d422400f3a5fc3faf710717
karmab <karimboumedhel@gmail.com>
2019-10-14 15:22:07 +0200

    20.0 RELEASE

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if os.path.exists('README.rst'):
 
 setup(
     name='kcli',
-    version='15.2',
+    version='20.0',
     include_package_data=True,
     packages=find_packages(),
     zip_safe=False,
