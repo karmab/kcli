049c2d3f89bc7405a1b0ac8644194abdfe93e3f9
karmab <karimboumedhel@gmail.com>
2019-11-05 18:12:37 +0100

    ubuntu1910

--- kvirt/defaults.py
+++ kvirt/defaults.py
@@ -53,7 +53,8 @@ IMAGES = {'arch': 'https://linuximages.de/openstack/arch/arch-openstack-LATEST-i
           'ubuntu1804': 'https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.img',
           'ubuntu1810': 'https://cloud-images.ubuntu.com/releases/cosmic/release-20190628/'
           'ubuntu-18.10-server-cloudimg-amd64.img',
-          'ubuntu1904': 'https://cloud-images.ubuntu.com/releases/disco/release/ubuntu-19.04-server-cloudimg-amd64.img'}
+          'ubuntu1904': 'https://cloud-images.ubuntu.com/releases/disco/release/ubuntu-19.04-server-cloudimg-amd64.img',
+          'ubuntu1910': 'https://cloud-images.ubuntu.com/releases/eoan/release/ubuntu-19.10-server-cloudimg-amd64.img'}
 IMAGESCOMMANDS = {'debian8': 'echo datasource_list: [NoCloud, ConfigDrive, Openstack, Ec2] > /etc/cloud/cloud.cfg.d/'
                   '90_dpkg.cfg'}
 REPORT = False
