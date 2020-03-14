5cebacab9fae278dd1dca8333d1aaa791c259611
karmab <karimboumedhel@gmail.com>
2020-03-14 01:05:28 +0100

    try fixing deb generation

--- packagecloud.sh
+++ packagecloud.sh
@@ -4,14 +4,10 @@ export DEB_BUILD_OPTIONS=nocheck debuild
 export VERSION="99.0"
 export MINOR="$(git rev-parse --short HEAD)"
 sudo apt-get update
-sudo apt-get -y install ruby-dev
-sudo gem install rake package_cloud
+sudo apt-get -y install build-essential fakeroot python3-setuptools python3-all debhelper curl
 find . -name *pyc -exec rm {} \;
 python3 setup.py --command-packages=stdeb.command sdist_dsc --debian-version $MINOR --depends python3-dateutil,python3-prettytable,python3-flask,python3-netaddr,python3-libvirt,genisoimage bdist_deb
-echo $PACKAGE_CLOUD_CREDS > .packagecloud
-package_cloud push karmab/kcli/debian/jessie deb_dist/*deb
-package_cloud push karmab/kcli/debian/stretch deb_dist/*deb
-package_cloud push karmab/kcli/debian/buster deb_dist/*deb
-package_cloud push karmab/kcli/ubuntu/cosmic deb_dist/*deb
-package_cloud push karmab/kcli/ubuntu/disco deb_dist/*deb
-package_cloud push karmab/kcli/ubuntu/eoan deb_dist/*deb
+deb=$(realpath $(find . -name *.deb))
+for id in 25 149 150 199 203 206 ; do
+  curl -F "package[distro_version_id]=$id" -F "package[package_file]=@$deb" https://$PACKAGE_CLOUD_TOKEN:@packagecloud.io/api/v1/repos/karmab/kcli/packages.json
+done
