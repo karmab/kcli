6a8abd8d104b3ef919b51ff50c9708c72df9cd84
karmab <karimboumedhel@gmail.com>
2020-03-16 11:27:21 +0100

    add jinja filter basename and dirname

--- kvirt/jinjafilters/__init__.py
+++ kvirt/jinjafilters/__init__.py
@@ -1,3 +1,14 @@
+import os
+
+
+def basename(path):
+    return os.path.basename(path)
+
+
+def dirname(path):
+    return os.path.dirname(path)
+
+
 def ocpnodes(cluster, platform, masters, workers):
     masters = ['%s-master-%d' % (cluster, num) for num in range(masters)]
     workers = ['%s-worker-%d' % (cluster, num) for num in range(workers)]
@@ -7,4 +18,4 @@ def ocpnodes(cluster, platform, masters, workers):
         return ["%s-bootstrap" % cluster] + masters + workers
 
 
-jinjafilters = {'ocpnodes': ocpnodes}
+jinjafilters = {'basename': basename, 'dirname': dirname, 'ocpnodes': ocpnodes}
