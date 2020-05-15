3c73005d1c3adebeca25e7846efd5b702a9611cc
karmab <karimboumedhel@gmail.com>
2020-05-15 18:34:36 +0200

    simplified travis pipeline

---
+++ travis/testing.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -ex
+
+image_pool_dir="/srv/pool/"
+sudo mkdir -p  "${image_pool_dir}"
+sudo chmod a+rwx "${image_pool_dir}"
+
+kcli="docker run --rm --privileged --net host -t -a stdout -a stderr -v ${HOME}/.kcli:/root/.kcli -v ${HOME}/.ssh:/root/.ssh -v /var/run/libvirt:/var/run/libvirt -v ${image_pool_dir}:${image_pool_dir} -v ${PWD}:/workdir karmab/kcli"
+
+ssh-keygen -t rsa -N ''
+sudo ${kcli} create plan -f pipeline/test_plan.yml -P image_pool_dir="${image_pool_dir}" test_plan
+sudo ${kcli} list vm
+sudo ${kcli} delete plan --yes test_plan
+tree -f /srv
