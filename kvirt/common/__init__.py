b7edb81997f8ad073b1569be03c9dd85d0cecfce
karmab <karimboumedhel@gmail.com>
2020-07-30 02:41:37 +0200

    typo

--- kvirt/common/__init__.py
+++ kvirt/common/__init__.py
@@ -1175,7 +1175,6 @@ def get_latest_rhcos(url, _type='kvm'):
 
 
 def get_commit_rhcos(commitid, _type='kvm'):
-    _type = 'vsphere'
     keys = {'ovirt': 'openstack', 'kvm': 'qemu', 'vsphere': 'vmware'}
     key = keys.get(_type, _type)
     buildurl = "https://raw.githubusercontent.com/openshift/installer/%s/data/data/rhcos.json" % commitid
