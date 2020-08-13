8e19b6f8876bced74df68833f2e321676ede9604
karmab <karimboumedhel@gmail.com>
2020-08-13 16:17:00 +0200

    apps generic: initial dashboard

---
+++ kvirt/kubeadm/apps/dashboard/install.sh
@@ -0,0 +1,8 @@
+export DASHBOARD_VERSION={{ 'kubernetes/dashboard' | latestversion }}
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/$DASHBOARD_VERSION/aio/deploy/recommended.yaml
+kubectl create -f ingress.yml
+kubectl create -f user.yml
+# kubectl patch svc -n kubernetes-dashboard kubernetes-dashboard -p '{"spec": {"type": "LoadBalancer"}}'
+TOKEN=$(kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep admin-user | awk '{print $1}') |grep 'token:' | awk -F: '{print $2}' | xargs)
+DASHBOARD_URL=https://$(kubectl get ingress -n default ingress-k8s-dashboard-service -o jsonpath='{.status.loadBalancer.ingress[0].ip}')/dashboard
+echo "Login to dashboard at $DASHBOARD_URL with token $TOKEN"
