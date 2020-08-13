a2ad21439de7fc622de2e410d7bb5c0bc4b9c465
karmab <karimboumedhel@gmail.com>
2020-08-13 02:26:35 +0200

    app argocd: address race conditions related to ingress

--- kvirt/kubeadm/extras_apps/argocd/install.sh
+++ kvirt/kubeadm/extras_apps/argocd/install.sh
@@ -1,5 +1,7 @@
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+echo Give sometime for ingress controller to get ready
+sleep 120
 kubectl apply -f ingress.yml
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
 ARGO_PASSWORD=$(kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server -o name | cut -d'/' -f 2)
