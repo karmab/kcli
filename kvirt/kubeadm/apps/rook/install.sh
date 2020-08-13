1bbe9952c5ba60e85792c3729a5ee8d0f58b9c11
karmab <karimboumedhel@gmail.com>
2020-08-13 12:28:45 +0200

    apps: rook in generic

--- kvirt/kubeadm/apps/rook/install.sh
+++ kvirt/kubeadm/apps/rook/install.sh
@@ -1,3 +1,4 @@
+set -euo pipefail
 export ROOK_VERSION={{ 'rook/rook' | latestversion }}
 kubectl create -f https://raw.githubusercontent.com/rook/rook/$ROOK_VERSION/cluster/examples/kubernetes/ceph/common.yaml
 kubectl create -f https://raw.githubusercontent.com/rook/rook/$ROOK_VERSION/cluster/examples/kubernetes/ceph/operator.yaml
