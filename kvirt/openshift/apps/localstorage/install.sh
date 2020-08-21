5b7845b6e94ee477cec97cba7bed302dc8c8f9ec
karmab <karimboumedhel@gmail.com>
2020-08-21 14:04:56 +0200

    openshift: localstorage support

---
+++ kvirt/openshift/apps/localstorage/install.sh
@@ -0,0 +1,15 @@
+oc create -f install.yml
+while true; do
+  oc get crd | grep localvolumes.local.storage.openshift.io 2>/dev/null && break;
+  echo "Waiting for CRD localvolumes.local.storage.openshift.io to be created"
+  sleep 15
+done
+oc create -f cr.yml
+while true; do
+  oc get sc/localstorage-sc 2>/dev/null && break;
+  echo "Waiting for the storageclass to be created"
+  sleep 5
+done
+oc patch storageclass localstorage-sc  -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+
+
