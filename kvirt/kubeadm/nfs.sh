78257a6ac99309292ab280365d829d9de0882ea7
karmab <karimboumedhel@gmail.com>
2020-09-04 20:30:36 +0200

    kube generic: fix nfs

--- kvirt/kubeadm/nfs.sh
+++ kvirt/kubeadm/nfs.sh
@@ -11,4 +11,10 @@ chmod 777 /pv${i}
 done
 exportfs -r
 systemctl start nfs-server ; systemctl enable nfs-server
+{% if masters > 1 %}
+IP="{{ api_ip }}"
+{% else %}
+IP=$(hostname -I | cut -d" " -f1)
+{% endif%}
+sed -i "s/IP/$IP/" /root/nfs.yml
 for i in `seq 1 20` ; do j=`printf "%03d" ${i}` ; sed "s/001/$j/" /root/nfs.yml | kubectl create -f - ; done
