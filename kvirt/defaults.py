30a757f6e879cf22e8016a72a6dd1082949aa649
karmab <karimboumedhel@gmail.com>
2020-09-04 13:15:00 +0200

    add image to metadata fields

--- kvirt/defaults.py
+++ kvirt/defaults.py
@@ -179,4 +179,4 @@ oEO8BRcXIiXiQqW9KnF99fXOiQ/cKYh3kWBBPnuEOhC77Ke5aMlqMNOPULf3PMix
 VIRTTYPE = None
 ZEROTIER = []
 CACHE = False
-METADATA_FIELDS = ['plan', 'profile', 'owner', 'dnsclient', 'domain', 'kube', 'kubetype', 'loadbalancer']
+METADATA_FIELDS = ['plan', 'image', 'profile', 'owner', 'dnsclient', 'domain', 'kube', 'kubetype', 'loadbalancer']
