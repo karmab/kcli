a557c6963212b24ed6912a1941c32cec0d5c3629
Yuval Kashtan <ykashtan@redhat.com>
2020-09-07 22:55:22 +0300

    add patch commands to enable baremetal support in acm (#229)
    https://github.com/open-cluster-management/deploy#enabling-bare-metal-consoles

--- kvirt/openshift/apps/acm/install.sh
+++ kvirt/openshift/apps/acm/install.sh
@@ -2,3 +2,10 @@ oc create -f install.yml
 sleep 10
 oc wait --for=condition=Ready pod -l name=multiclusterhub-operator -n open-cluster-management
 oc create -f cr.yml
+
+# Enabling Bare metal consoles
+oc -n open-cluster-management patch deploy console-header -p '{"spec":{"template":{"spec":{"containers":[{"name":"console-header","env":
+[{"name": "featureFlags_baremetal","value":"true"}]}]}}}}'
+DEPLOY_NAME=`oc -n open-cluster-management get deploy -o name | grep consoleui`
+oc -n open-cluster-management patch ${DEPLOY_NAME} -p '{"spec":{"template":{"spec":{"containers":[{"name":"hcm-ui","env":
+[{"name": "featureFlags_baremetal","value":"true"}]}]}}}}'
