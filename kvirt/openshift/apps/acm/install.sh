09fc5c7affba6c6a0891788fb1e015d96e28d489
karmab <karimboumedhel@gmail.com>
2020-09-07 21:56:41 +0200

    openshift acm: separate lines

--- kvirt/openshift/apps/acm/install.sh
+++ kvirt/openshift/apps/acm/install.sh
@@ -4,8 +4,6 @@ oc wait --for=condition=Ready pod -l name=multiclusterhub-operator -n open-clust
 oc create -f cr.yml
 
 # Enabling Bare metal consoles
-oc -n open-cluster-management patch deploy console-header -p '{"spec":{"template":{"spec":{"containers":[{"name":"console-header","env":
-[{"name": "featureFlags_baremetal","value":"true"}]}]}}}}'
-DEPLOY_NAME=`oc -n open-cluster-management get deploy -o name | grep consoleui`
-oc -n open-cluster-management patch ${DEPLOY_NAME} -p '{"spec":{"template":{"spec":{"containers":[{"name":"hcm-ui","env":
-[{"name": "featureFlags_baremetal","value":"true"}]}]}}}}'
+oc -n open-cluster-management patch deploy console-header -p '{"spec":{"template":{"spec":{"containers":[{"name":"console-header","env":[{"name": "featureFlags_baremetal","value":"true"}]}]}}}}'
+DEPLOY_NAME=$(oc -n open-cluster-management get deploy -o name | grep consoleui)
+oc -n open-cluster-management patch ${DEPLOY_NAME} -p '{"spec":{"template":{"spec":{"containers":[{"name":"hcm-ui","env":[{"name": "featureFlags_baremetal","value":"true"}]}]}}}}'
