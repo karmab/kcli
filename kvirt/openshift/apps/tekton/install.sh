016daaed16971a43a38b46f6417491294a8ca8fb
karmab <karimboumedhel@gmail.com>
2020-09-08 18:02:05 +0200

    openshift: tekton olm install

--- kvirt/openshift/apps/tekton/install.sh
+++ kvirt/openshift/apps/tekton/install.sh
@@ -1,3 +1,1 @@
-oc new-project tekton-pipelines
-oc adm policy add-scc-to-user anyuid -z tekton-pipelines-controller
-oc apply --filename https://storage.googleapis.com/tekton-releases/pipeline/latest/release.notags.yaml
+oc create -f install.yml
---
+++ kvirt/openshift/apps/tekton/uninstall.sh
@@ -0,0 +1,1 @@
+oc delete -f install.yml
