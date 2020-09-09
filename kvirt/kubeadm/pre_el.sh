551425ce9f235a08d1e66874b0ff54a744759945
karmab <karimboumedhel@gmail.com>
2020-09-09 10:09:53 +0200

    kube generic: default to centos8

--- kvirt/kubeadm/pre_el.sh
+++ kvirt/kubeadm/pre_el.sh
@@ -4,17 +4,16 @@ baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
 enabled=1
 gpgcheck=0
 repo_gpgcheck=0""" >/etc/yum.repos.d/kubernetes.repo
-yum -y install git
 echo net.bridge.bridge-nf-call-iptables=1 >> /etc/sysctl.d/99-sysctl.conf
 sysctl -p
 setenforce 0
 sed -i "s/SELINUX=enforcing/SELINUX=permissive/" /etc/selinux/config
 {%- if version != None %}
-VERSION=$(yum --showduplicates list kubectl  | grep kubectl | grep {{ version }} | tail -1 | awk '{print $2}' | xargs)
+VERSION=$(dnf --showduplicates list kubectl  | grep kubectl | grep {{ version }} | tail -1 | awk '{print $2}' | xargs)
 {%- else %}
-VERSION=$(yum --showduplicates list kubectl  | grep kubectl | tail -1 | awk '{print $2}' | xargs)
+VERSION=$(dnf --showduplicates list kubectl  | grep kubectl | tail -1 | awk '{print $2}' | xargs)
 {%- endif %}
-yum install -y docker kubelet-$VERSION kubectl-$VERSION kubeadm-$VERSION
-sed -i "s/--selinux-enabled //" /etc/sysconfig/docker
-systemctl enable docker && systemctl start docker
-systemctl enable kubelet && systemctl start kubelet
+dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+dnf -y install -y docker-ce kubelet-$VERSION kubectl-$VERSION kubeadm-$VERSION git --nobest
+systemctl enable --now docker
+systemctl enable --now kubelet
--- kvirt/kubeadm/pre_ubuntu.sh
+++ kvirt/kubeadm/pre_ubuntu.sh
@@ -1,4 +1,4 @@
-apt-get update && apt-get install -y apt-transport-https curl
+apt-get update && apt-get -y install apt-transport-https curl git
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
@@ -9,6 +9,6 @@ VERSION=$(apt-cache show kubectl | grep Version | grep {{ version }} | head -1 |
 {% else %}
 VERSION=$(apt-cache show kubectl | grep Version | head -1 | awk -F: '{print $2}' | xargs)
 {% endif %}
-apt-get install -y docker.io kubelet=$VERSION kubectl=$VERSION kubeadm=$VERSION
-systemctl enable docker && systemctl start docker
-systemctl enable kubelet && systemctl start kubelet
+apt-get -y install docker.io kubelet=$VERSION kubectl=$VERSION kubeadm=$VERSION
+systemctl enable --now docker
+systemctl enable --now kubelet
