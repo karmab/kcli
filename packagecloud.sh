3d991a7bf7d7e6dbc3b67fb9bb4c14adb774ea3a
karmab <karimboumedhel@gmail.com>
2020-11-15 12:21:35 +0100

    fix package cloud

--- packagecloud.sh
+++ packagecloud.sh
@@ -4,7 +4,7 @@ export DEB_BUILD_OPTIONS=nocheck debuild
 export VERSION="99.0"
 export MINOR="$(git rev-parse --short HEAD)"
 sudo apt-get update
-sudo apt-get -y install build-essential fakeroot python3-setuptools python3-all debhelper curl
+sudo apt-get -y install build-essential fakeroot python3-setuptools python3-all debhelper curl genisoimage
 find . -name *pyc -exec rm {} \;
 curl -s https://github.com/karmab/kcli/commits/master | grep 'https://github.com/karmab/kcli/commits/master?' | sed 's@.*=\(.......\).*+.*@\1@' > kvirt/version/git > kvirt/version/git
 python3 setup.py --command-packages=stdeb.command sdist_dsc --debian-version $MINOR --depends python3-dateutil,python3-prettytable,python3-flask,python3-netaddr,python3-libvirt,python3-requests genisoimage bdist_deb
