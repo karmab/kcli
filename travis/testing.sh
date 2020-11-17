06e5d6a7844d7577ead640a79273b0fc89649009
karmab <karimboumedhel@gmail.com>
2020-11-17 11:49:20 +0100

    fix travis

--- travis/testing.sh
+++ travis/testing.sh
@@ -6,7 +6,7 @@ image_pool_dir="/srv/pool/"
 sudo mkdir -p  "${image_pool_dir}"
 sudo chmod a+rwx "${image_pool_dir}"
 
-kcli="docker run --rm --privileged --net host -t -a stdout -a stderr -v ${HOME}/.kcli:/root/.kcli -v ${HOME}/.ssh:/root/.ssh -v /var/run/libvirt:/var/run/libvirt -v ${image_pool_dir}:${image_pool_dir} -v ${PWD}:/workdir karmab/kcli"
+kcli="docker run --rm --privileged --net host -t -a stdout -a stderr -v ${HOME}/.kcli:/root/.kcli -v ${HOME}/.ssh:/root/.ssh -v /var/run/libvirt:/var/run/libvirt -v ${image_pool_dir}:${image_pool_dir} -v ${PWD}:/workdir quay.io/karmab/kcli"
 
 ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa
 sudo ${kcli} create plan -f travis/test_plan.yml -P image_pool_dir="${image_pool_dir}" test_plan
