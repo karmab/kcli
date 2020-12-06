8c179c97fd5faac047666f64715f72da29ee87e1
karmab <karimboumedhel@gmail.com>
2020-12-06 01:51:06 +0100

    kubeadm: fix

--- kvirt/kubeadm/pre_el.sh
+++ kvirt/kubeadm/pre_el.sh
@@ -5,6 +5,7 @@ enabled=1
 gpgcheck=0
 repo_gpgcheck=0""" >/etc/yum.repos.d/kubernetes.repo
 echo net.bridge.bridge-nf-call-iptables=1 >> /etc/sysctl.d/99-sysctl.conf
+modprobe br_netfilter
 sysctl -p
 setenforce 0
 sed -i "s/SELINUX=enforcing/SELINUX=permissive/" /etc/selinux/config
@@ -14,32 +15,9 @@ VERSION=$(dnf --showduplicates list kubectl  | grep kubectl | grep {{ version }}
 VERSION=$(dnf --showduplicates list kubectl  | grep kubectl | tail -1 | awk '{print $2}' | xargs)
 {% endif %}
 
-modprobe overlay
-modprobe br_netfilter
-cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
-net.bridge.bridge-nf-call-iptables  = 1
-net.ipv4.ip_forward                 = 1
-net.bridge.bridge-nf-call-ip6tables = 1
-EOF
-sysctl --system
-
-{% if engine == 'docker' %}
 dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
-dnf -y install -y docker-ce --nobest
+dnf -y install -y docker-ce iptables --nobest
+export PATH=/sbin:$PATH
 systemctl enable --now docker
-{% elif engine == 'containerd' %}
-dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
-dnf -y install -y containerd.io
-mkdir -p /etc/containerd
-containerd config default > /etc/containerd/config.toml
-systemctl enable --now containerd
-{% else %}
-OS="CentOS_8_Stream"
-curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable.repo https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/devel:kubic:libcontainers:stable.repo
-curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable:cri-o:$VERSION.repo https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$VERSION/$OS/devel:kubic:libcontainers:stable:cri-o:$VERSION.repo
-dnf -y install cri-o
-systemctl enable --now crio
-{% endif %}
-
 dnf -y install -y kubelet-$VERSION kubectl-$VERSION kubeadm-$VERSION git
 systemctl enable --now kubelet
--- kvirt/kubeadm/pre_ubuntu.sh
+++ kvirt/kubeadm/pre_ubuntu.sh
@@ -10,31 +10,7 @@ VERSION=$(apt-cache show kubectl | grep Version | grep {{ version }} | head -1 |
 VERSION=$(apt-cache show kubectl | grep Version | head -1 | awk -F: '{print $2}' | xargs)
 {% endif %}
 
-{% if engine == 'docker' %}
 apt-get -y install docker.io
 systemctl enable --now docker
-{% elif engine == 'containerd' %}
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key --keyring /etc/apt/trusted.gpg.d/docker.gpg add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update 
-apt-get install -y containerd.io
-mkdir -p /etc/containerd
-containerd config default > /etc/containerd/config.toml
-systemctl enable --now containerd
-{% else %}
-OS="xUbuntu_20.04"
-cat <<EOF | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
-deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /
-EOF
-cat <<EOF | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$VERSION.list
-deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/ /
-EOF
-curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/libcontainers.gpg add -
-curl -L https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$VERSION/$OS/Release.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/libcontainers-cri-o.gpg add -
-apt-get update
-apt-get install cri-o cri-o-runc
-systemctl enable --now crio
-{% endif %}
-
 apt-get -y install kubelet=$VERSION kubectl=$VERSION kubeadm=$VERSION
 systemctl enable --now kubelet
