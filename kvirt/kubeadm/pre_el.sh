472c314004e2704214f004867c8b0cffb16becb9
karmab <karimboumedhel@gmail.com>
2020-12-06 22:27:03 +0100

    kubeadm: containerd support

--- kvirt/kubeadm/pre_el.sh
+++ kvirt/kubeadm/pre_el.sh
@@ -15,9 +15,37 @@ VERSION=$(dnf --showduplicates list kubectl  | grep kubectl | grep {{ version }}
 VERSION=$(dnf --showduplicates list kubectl  | grep kubectl | tail -1 | awk '{print $2}' | xargs)
 {% endif %}
 
+{% if engine == 'docker' %}
 dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
 dnf -y install -y docker-ce iptables --nobest
 export PATH=/sbin:$PATH
 systemctl enable --now docker
+{% else %}
+modprobe overlay
+modprobe br_netfilter
+cat <<EOF | tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+sysctl --system
+{% if engine == 'crio' %}
+OS="CentOS_8"
+CRIO_VERSION=$(echo $VERSION | cut -d. -f1,2)
+curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable.repo https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/devel:kubic:libcontainers:stable.repo
+curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable:cri-o:$CRIO_VERSION.repo https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$CRIO_VERSION/$OS/devel:kubic:libcontainers:stable:cri-o:$CRIO_VERSION.repo
+yum -y install cri-o
+systemctl enable --now crio
+{% else %}
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install -y containerd.io
+mkdir -p /etc/containerd
+containerd config default > /etc/containerd/config.toml
+systemctl enable --now containerd
+systemctl restart containerd
+{% endif %}
+{% endif %}
+
 dnf -y install -y kubelet-$VERSION kubectl-$VERSION kubeadm-$VERSION git
 systemctl enable --now kubelet
--- kvirt/kubeadm/pre_ubuntu.sh
+++ kvirt/kubeadm/pre_ubuntu.sh
@@ -10,7 +10,41 @@ VERSION=$(apt-cache show kubectl | grep Version | grep {{ version }} | head -1 |
 VERSION=$(apt-cache show kubectl | grep Version | head -1 | awk -F: '{print $2}' | xargs)
 {% endif %}
 
+{% if engine == 'docker' %}
 apt-get -y install docker.io
 systemctl enable --now docker
+{% else %}
+modprobe overlay
+modprobe br_netfilter
+cat <<EOF | tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+sysctl --system
+{% if engine == 'crio' %}
+OS="xUbuntu_20.04"
+CRIO_VERSION=$(echo $VERSION | cut -d. -f1,2)
+cat <<EOF | tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /
+EOF
+cat <<EOF | tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$CRIO_VERSION.list
+deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$CRIO_VERSION/$OS/ /
+EOF
+curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | apt-key --keyring /etc/apt/trusted.gpg.d/libcontainers.gpg add -
+curl -L https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$CRIO_VERSION/$OS/Release.key | apt-key --keyring /etc/apt/trusted.gpg.d/libcontainers-cri-o.gpg add -
+apt-get update
+apt-get install cri-o cri-o-runc
+systemctl restart crio
+{% else %}
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key --keyring /etc/apt/trusted.gpg.d/docker.gpg add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get install -y containerd.io
+mkdir -p /etc/containerd
+containerd config default > /etc/containerd/config.toml
+systemctl restart containerd
+{% endif %}
+{% endif %}
 apt-get -y install kubelet=$VERSION kubectl=$VERSION kubeadm=$VERSION
 systemctl enable --now kubelet
