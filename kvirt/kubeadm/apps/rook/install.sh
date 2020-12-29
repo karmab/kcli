a58a5707b3742d9f98126f35dbf4054e7486c29c
karmab <karimboumedhel@gmail.com>
2020-12-29 15:50:16 +0100

    kube app: fix rook

--- kvirt/kubeadm/apps/rook/install.sh
+++ kvirt/kubeadm/apps/rook/install.sh
@@ -1,5 +1,6 @@
 set -euo pipefail
 export ROOK_VERSION={{ 'rook/rook' | githubversion(rook_version) }}
+kubectl create -f https://raw.githubusercontent.com/rook/rook/$ROOK_VERSION/cluster/examples/kubernetes/ceph/crds.yaml
 kubectl create -f https://raw.githubusercontent.com/rook/rook/$ROOK_VERSION/cluster/examples/kubernetes/ceph/common.yaml
 kubectl create -f https://raw.githubusercontent.com/rook/rook/$ROOK_VERSION/cluster/examples/kubernetes/ceph/operator.yaml
 kubectl create -f https://raw.githubusercontent.com/rook/rook/$ROOK_VERSION/cluster/examples/kubernetes/ceph/cluster.yaml
