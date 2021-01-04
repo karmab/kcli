aa3936b523d1e2802c983f9f59b40148eb2c20a2
karmab <karimboumedhel@gmail.com>
2021-01-05 00:02:00 +0100

    kube k3s: support disabling components

--- kvirt/k3s/bootstrap.sh
+++ kvirt/k3s/bootstrap.sh
@@ -1,9 +1,13 @@
+{% set extra_args = [] %}
+{% for component in disabled_components %}
+{% do extra_args.append("--disable " + component) %}
+{% endfor %}
 apt-get -y install curl
 {% if masters > 1 %}
-curl -sfL https://get.k3s.io | K3S_TOKEN={{ token }} sh -s - server --cluster-init
+curl -sfL https://get.k3s.io | K3S_TOKEN={{ token }} sh -s - server --cluster-init {{ extra_args|join(" ") }}
 export IP={{ api_ip }}
 {% else %}
-curl -sfL https://get.k3s.io | sh -
+curl -sfL https://get.k3s.io | sh - server {{ extra_args|join(" ") }}
 export IP=$(hostname -I | cut -f1 -d" ")
 {% endif %}
 export K3S_TOKEN=$(cat /var/lib/rancher/k3s/server/node-token)
