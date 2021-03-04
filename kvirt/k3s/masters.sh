b4cdb8142a271e15512a2ca6730fb785666ca97a
karmab <karimboumedhel@gmail.com>
2021-03-04 22:00:29 +0100

    disable for extra masters

--- kvirt/k3s/masters.sh
+++ kvirt/k3s/masters.sh
@@ -1,2 +1,6 @@
+{% set extra_args = [] %}
+{% for component in disabled_components %}
+{% do extra_args.append("--disable " + component) %}
+{% endfor %}
 apt-get -y install curl
-curl -sfL https://get.k3s.io | K3S_TOKEN={{ token }} K3S_URL=https://{{ api_ip }}:6443 sh -s - server
+curl -sfL https://get.k3s.io | K3S_TOKEN={{ token }} K3S_URL=https://{{ api_ip }}:6443 sh -s - server {{ extra_args|join(" ") }}
