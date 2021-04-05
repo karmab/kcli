3265c7b759d34d74429e1f98e287e440f49d40e8
Lars Bengtsson <larssb@users.noreply.github.com>
2021-04-05 20:41:12 +0200

    Adds support for INSTALL_K3S_VERSION
    - so that a specific version can be installed.
    Good for rolling back to a prev. version. And
    other related needs.
    - updates kcli_default info
    - the parameters now also directly map to
    the RancherLabs K3S https://rancher.com/docs/k3s/latest/en/installation/install-options/#options-for-installation-with-script
    script install options
    - masters.sh also reflects K3S version and channel vars

--- kvirt/k3s/masters.sh
+++ kvirt/k3s/masters.sh
@@ -7,4 +7,4 @@ apt-get -y install curl
 echo bpffs /sys/fs/bpf bpf defaults 0 0 >> /etc/fstab
 mount /sys/fs/bpf
 {% endif %}
-curl -sfL https://get.k3s.io | {{ "INSTALL_K3S_EXEC='--disable-network-policy --no-flannel'" if sdn != "flannel" else '' }} K3S_TOKEN={{ token }} K3S_URL=https://{{ api_ip }}:6443 sh -s - server {{ extra_args|join(" ") }}
+curl -sfL https://get.k3s.io | {{ "INSTALL_K3S_EXEC='--disable-network-policy --no-flannel'" if sdn != "flannel" else '' }} INSTALL_K3S_CHANNEL={{ install_k3s_channel }} {{ INSTALL_K3S_VERSION=install_k3s_version if install_k3s_version != null || install_k3s_version != '' }} K3S_TOKEN={{ token }} K3S_URL=https://{{ api_ip }}:6443 sh -s - server {{ extra_args|join(" ") }}
