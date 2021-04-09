87e0715a9219f0dee917eaaa4cdedd9164743e03
karmab <karimboumedhel@gmail.com>
2021-04-09 20:49:36 +0200

    disconnected: sync_image path

--- kvirt/openshift/disconnected/extras.sh
+++ kvirt/openshift/disconnected/extras.sh
@@ -1,5 +1,5 @@
 images="quay.io/karmab/curl:latest quay.io/openshift/origin-coredns:latest quay.io/karmab/haproxy:latest quay.io/openshift/origin-keepalived-ipfailover:latest quay.io/openshift-metal3/mdns-publisher:latest quay.io/karmab/kubectl:latest"
 
 for image in $images ; do
-  sync_image.sh $image
+  /root/bin/sync_image.sh $image
 done
