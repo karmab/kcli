73027d0188e7f2747f9734e5fa50d63ea4c0dd24
Leo Ochoa <lochoa@redhat.com>
2021-07-21 09:48:02 +0200

    Add support for PaC controllers
    Adding current state-of-the-art Policy-as-Code (PaC) Controllers like OPA/Gatekeeper and Kyverno to handle policies on generic k8s clusters.

--- kvirt/kubeadm/masters.sh
+++ kvirt/kubeadm/masters.sh
@@ -49,6 +49,14 @@ kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/mast
 {% endif %}
 {% endif %}
 
+{% if policy_as_code %}
+{% if policy_as_code_method == 'gatekeeper' %}
+kubectl apply -f https://raw.githubusercontent.com/open-policy-agent/gatekeeper/master/deploy/gatekeeper.yaml
+{% elif policy_as_code_method == 'kyverno' %}
+kubectl apply -f https://raw.githubusercontent.com/kyverno/kyverno/main/definitions/release/install.yaml
+{% endif %}
+{% endif %}
+
 {% if autolabel %}
 kubectl apply -f https://raw.githubusercontent.com/karmab/autolabeller/master/autorules.yml
 {% endif %}
