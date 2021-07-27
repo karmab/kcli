9a5fe520d5dc17e6992b90f9671ff23629d2393a
karmab <karimboumedhel@gmail.com>
2021-07-27 12:46:20 +0200

    fix gha

--- .github/prep.sh
+++ .github/prep.sh
@@ -2,6 +2,3 @@ pip3 install pep8 misspellings stdeb requests
 pip3 install setuptools --upgrade
 apt-get update
 apt-get -y install libvirt-daemon libvirt0 qemu-system-x86 qemu-utils qemu-kvm tree
-
-./setup_hypervisor.sh
-
