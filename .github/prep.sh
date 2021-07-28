c64307d6a961eb4b9ea2a122cffeef47792b3781
karmab <karimboumedhel@gmail.com>
2021-07-28 12:58:51 +0200

    fix gha

--- .github/prep.sh
+++ .github/prep.sh
@@ -2,4 +2,5 @@ pip3 install pep8 misspellings stdeb requests
 pip3 install setuptools --upgrade
 pip3 install wheel
 apt-get update
-apt-get -y install libvirt-daemon libvirt0 qemu-system-x86 qemu-utils qemu-kvm libvirt-daemon-system
+apt-get -y install libvirt-daemon libvirt0 qemu-system-x86 qemu-utils qemu-kvm libvirt-daemon-system build-essential fakeroot python3-setuptools python3-all debhelper curl genisoimage dh-python python3-distutils
+pip3 install cloudsmith-cli
