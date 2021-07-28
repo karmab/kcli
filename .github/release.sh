da9043cc9b3694f41e34b9e53df9f214fa858784
karmab <karimboumedhel@gmail.com>
2021-07-28 11:25:11 +0200

    fix gha

--- .github/release.sh
+++ .github/release.sh
@@ -5,6 +5,6 @@ set -ex
 docker login -u $QUAY_USERNAME -p $QUAY_PASSWORD quay.io
 docker push quay.io/karmab/kcli:latest
 
-"./cloudsmith.sh"
-"./cloudsmith_clean.sh"
+#"./cloudsmith.sh"
+#"./cloudsmith_clean.sh"
 "./pypi.sh"
