311c905f510a998a8cd179c55b1629c2bc857410
karmab <karimboumedhel@gmail.com>
2021-07-29 16:21:26 +0200

    improve test coverage

--- .github/hypervisor.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo usermod -aG libvirt $(id -un)
-sudo newgrp libvirt
--- .github/testing.sh
+++ .github/testing.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 set -euo pipefail
-
-pip3 install -e .
+export PATH=/usr/local/bin:$PATH
 sudo mkdir -p /var/lib/libvirt/images
 sudo setfacl -m u:$(id -un):rwx /var/lib/libvirt/images
 mkdir ~/.kcli
 ssh-keygen -t rsa -N '' -f ~/.kcli/id_rsa
-kcli create pool -p /var/lib/libvirt/images default
+pip3 install -e .
+#kcli create pool -p /var/lib/libvirt/images default
 #kcli create plan -f .github/test_plan.yml test_plan
-#kcli list plan | grep -q test_plan
+#kcli list plan | grep test_plan
 #kcli delete plan --yes test_plan
-kcli list vm
