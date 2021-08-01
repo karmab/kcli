12ecad7c0d14f8d46a19e8009b3fba9897f0c1ba
karmab <karimboumedhel@gmail.com>
2021-08-01 19:38:25 +0200

    reenable kcli tests in ci

--- .github/prep.sh
+++ .github/prep.sh
@@ -3,5 +3,4 @@ apt-get -y install python3-pip python3-wheel python3-setuptools python3-all pyth
 pip3 install misspellings cloudsmith-cli pep8 stdeb
 pip3 install -U Jinja2
 apt-get -y install libvirt-daemon libvirt0 qemu-system-x86 qemu-utils qemu-kvm libvirt-daemon-system curl genisoimage python3-libvirt
-usermod -aG libvirt runner
-newgrp libvirt
+setfacl -m u:runner:rwx /var/run/libvirt/libvirt-sock
