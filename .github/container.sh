2543a6163e4ea69b07280f4ddc84148a6cbc9990
karmab <karimboumedhel@gmail.com>
2021-08-05 10:38:13 +0200

    push container image with tag from git commit

--- .github/container.sh
+++ .github/container.sh
@@ -2,9 +2,12 @@
 
 set -ex
 
-git rev-parse --short HEAD > kvirt/version/git
+TAG=$(git rev-parse --short HEAD)
+echo $TAG > kvirt/version/git
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD docker.io
 docker build -t quay.io/karmab/kcli -f extras/alpine .
+docker tag quay.io/karmab/kcli:latest quay.io/karmab/kcli:$TAG
 
 docker login -u $QUAY_USERNAME -p $QUAY_PASSWORD quay.io
 docker push quay.io/karmab/kcli:latest
+docker push quay.io/karmab/kcli:$TAG
