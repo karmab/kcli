5383571cd26eafa0a22aaca4fef96dec4b797d83
larssb <larssb@users.noreply.github.com>
2021-08-17 21:27:03 +0200

    Worker nodes shouldn't get network related settings
    As worker nodes/agents in K3s gets their network settings from masters network settings shouldn't be propagated to the workers/agents.
    
    See [this](https://rancher-users.slack.com/archives/CGGQEHPPW/p1629227659194400?thread_ts=1629225486.193200&cid=CGGQEHPPW) comments from Mr. Brad Davidson from RancherLabs: `agents get their network config from the servers`
    
    ⚡ 👍

--- kvirt/k3s/__init__.py
+++ kvirt/k3s/__init__.py
@@ -121,8 +121,7 @@ def create(config, plandir, cluster, overrides):
     with open("%s/join.sh" % clusterdir, 'w') as f:
         if api_ip is None:
             api_ip = k.info(firstmaster)['ip']
-        joincmd = "curl -sfL https://get.k3s.io | %s K3S_URL=https://%s:6443 K3S_TOKEN=%s" % (nodes_install_k3s_args,
-                                                                                              api_ip, token)
+        joincmd = "curl -sfL https://get.k3s.io | K3S_URL=https://%s:6443 K3S_TOKEN=%s" % (api_ip, token)
         extra_args = data['extra_worker_args'] if data.get('extra_worker_args', []) else data.get('extra_args', [])
         extra_args = ' '.join(extra_args)
         f.write("%s sh -s - agent %s \n" % (joincmd, extra_args))
