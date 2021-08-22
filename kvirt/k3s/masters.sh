547f525d08dbaba3522a157e7c9bb6b039482088
LarsBingBong <80959443+LarsBingBong@users.noreply.github.com>
2021-08-22 20:22:38 +0200

    No special treatment for the Cilium CNI
    We previously talked about this. I've personally just overlooked the lines relative to the Cilium SDN/CNI in this file. This commit reflects that.

--- kvirt/k3s/masters.sh
+++ kvirt/k3s/masters.sh
@@ -3,9 +3,5 @@
 {% endif %}
 
 apt-get -y install curl
-{% if sdn != None and sdn == 'cilium' %}
-echo bpffs /sys/fs/bpf bpf defaults 0 0 >> /etc/fstab
-mount /sys/fs/bpf
-{% endif %}
 curl -sfL https://get.k3s.io | {{ install_k3s_args }} K3S_TOKEN={{ token }} K3S_URL=https://{{ api_ip }}:6443 sh -s - server {{ extra_args|join(" ") }}
 apt-get -y remove curl
