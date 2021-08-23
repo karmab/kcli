bbb904839b9fd3331b64aa212825a46d1f840c04
LarsBingBong <80959443+LarsBingBong@users.noreply.github.com>
2021-08-23 23:40:24 +0200

    Fix issue of extra masters mistaken as K3s agents
    The fix is to go away from the use of the K3S_URL. Specifying that apparently makes the K3s binary interpret the install cmd as being for a K3s agent/worker node. Whereas using `--serer https://serverIP:6443` makes the K3s binary properly interpret the install cmd as being for a K3s master/control-plane node.
    
    Tested on local and works for both a CNI that is not flannel and when specifying `sdn: flannel`
    
    Give me a merge 😉 🍻

--- kvirt/k3s/masters.sh
+++ kvirt/k3s/masters.sh
@@ -7,5 +7,5 @@ apt-get -y install curl
 echo bpffs /sys/fs/bpf bpf defaults 0 0 >> /etc/fstab
 mount /sys/fs/bpf
 {% endif %}
-curl -sfL https://get.k3s.io | {{ install_k3s_args }} K3S_TOKEN={{ token }} K3S_URL=https://{{ api_ip }}:6443 sh -s - server {{ extra_args|join(" ") }}
+curl -sfL https://get.k3s.io | {{ install_k3s_args }} K3S_TOKEN={{ token }} sh -s - server --server https://{{ api_ip }}:6443 {{ extra_args|join(" ") }}
 apt-get -y remove curl
