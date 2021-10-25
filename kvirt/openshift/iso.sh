28dde6b6cad421b850c5c1e81a88c8b9de3b47ed
karmab <karimboumedhel@gmail.com>
2021-10-25 23:38:52 +0200

    use the same logic in create openshift-iso that for sno

--- kvirt/openshift/iso.sh
+++ kvirt/openshift/iso.sh
@@ -5,19 +5,20 @@
 {% do nics.append("ip=" + nic + ":off") %}
 {% endfor %}
 {% endif %}
-firstboot_args='console=tty0 rd.neednet=1 {{ nics | join(" ") }} {{ extra_args|default("") }}'
+
 for vg in $(vgs -o name --noheadings) ; do vgremove -y $vg ; done
 for pv in $(pvs -o name --noheadings) ; do pvremove -y $pv ; done
-if [ -b /dev/vda ]; then
-  install_device='/dev/vda'
-elif [ -b /dev/sda ]; then
-  install_device='/dev/sda'
-elif [ -b /dev/nvme0 ]; then
-  install_device='/dev/nvme0'
-else
+{% if install_disk is defined %}
+install_device='/dev/{{ install_disk | basename }}'
+{% else %}
+install_device=/dev/$(lsblk | grep disk | head -1 | cut -d" " -f1)
+if [ "$install_device" == "/dev/" ]; then
   echo "Can't find appropriate device to install to"
   exit 1
 fi
+{% endif %}
+
+firstboot_args='console=tty0 rd.neednet=1 {{ nics | join(" ") }} {{ extra_args|default("") }}'
 
 {% if ip is defined and netmask is defined and gateway is defined %}
 firstboot_args="$firstboot_args ip={{ "[" + ip + "]" if ':' in ip else ip }}::{{ "[" + gateway + "]" if ':' in gateway else gateway }}:{{ netmask }}:{{ hostname|default("") }}:{{ nic|default("ens3") }}:none nameserver={{ "[" + dns|default(gateway) + "]" if ':' in dns|default(gateway) else dns|default(gateway) }}"
