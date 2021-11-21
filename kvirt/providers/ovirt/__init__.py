c00618bef1233d1976f01a370a829d1e1ba91763
karmab <karimboumedhel@gmail.com>
2021-11-21 20:47:29 +0100

    ovirt: remove helpers.py file

--- kvirt/providers/ovirt/__init__.py
+++ kvirt/providers/ovirt/__init__.py
@@ -8,7 +8,6 @@ from distutils.spawn import find_executable
 from kvirt import common
 from kvirt.common import error, pprint, warning, get_ssh_pub_key
 from kvirt.defaults import UBUNTUS, METADATA_FIELDS
-from kvirt.providers.ovirt.helpers import IMAGES as oimages
 import ovirtsdk4 as sdk
 from ovirtsdk4 import Error as oerror
 import ovirtsdk4.types as types
@@ -23,6 +22,22 @@ import ssl
 from urllib.parse import urlparse
 from random import choice
 from string import ascii_lowercase
+
+oimages = {'CentOS-6-x86_64-GenericCloud.qcow2': 'CentOS 6 Generic Cloud Image',
+           'CentOS-Atomic-Host-7-GenericCloud.qcow2': 'CentOS 7 Atomic Host Image',
+           'CentOS-7-x86_64-GenericCloud.qcow2': 'CentOS 7 Generic Cloud Image',
+           'cirros-0.4.0-x86_64-disk.img': 'CirrOS 0.4.0 for x86_64',
+           'Fedora-Cloud-Base-24-1.2.x86_64.qcow2': 'Fedora 24 Cloud Base Image v20160921.0 for x86_64',
+           'Fedora-Cloud-Base-25-1.3.x86_64.qcow2': 'Fedora 25 Cloud Base Image v20170106.0 for x86_64',
+           'Fedora-Cloud-Base-26-1.5.x86_64.qcow2': 'Fedora 26 Cloud Base Image v1.5 for x86_64',
+           'Fedora-Cloud-Base-27-1.6.x86_64.qcow2': 'Fedora 27 Cloud Base Image v1.6 for x86_64',
+           'Fedora-Cloud-Base-28-1.1.x86_64.qcow2': 'Fedora 28 Cloud Base Image v1.1 for x86_64',
+           'trusty-server-cloudimg-amd64-disk1.img':
+           'Ubuntu Server 14.04 LTS (Trusty Tahr) Cloud Image v20170110 for x86_64',
+           'xenial-server-cloudimg-amd64-disk1.img':
+           'Ubuntu Server 16.04 LTS (Xenial Xerus) Cloud Image v20170111 for x86_64',
+           'yakkety-server-cloudimg-amd64-disk1.img':
+           'Ubuntu Server 16.10 (Yakkety Yak) Cloud Image v20170106 for x86_64'}
 
 
 class KOvirt(object):
