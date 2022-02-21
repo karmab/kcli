31ec3b68fda5c6cf712deeb05ec956a075457eda
karmab <karimboumedhel@gmail.com>
2022-02-21 09:24:03 +0100

    openshift app: ocs_default_storageclass variable

--- kvirt/openshift/apps/ocs-operator/post.sh
+++ kvirt/openshift/apps/ocs-operator/post.sh
@@ -1,2 +1,11 @@
-echo Run the following command to make ocs your default storage class
+{% if ocs_default_storageclass %}
+while true; do
+  oc get sc/ocs-storagecluster-ceph-rbd 2>/dev/null && break;
+  echo "Waiting for the storageclass to be created"
+  sleep 5
+done
+ oc patch storageclass ocs-storagecluster-ceph-rbd -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+{% else %}
+echo Run the following command to make ocs-storagecluster-ceph-rbd the default storage class
 echo oc patch storageclass ocs-storagecluster-ceph-rbd -p \'{\"metadata\": {\"annotations\":{\"storageclass.kubernetes.io/is-default-class\":\"true\"}}}\'
+{% endif %}
