4d6c2d1098f1316dff6828e7bdbea4bb761a686d
Karim Boumedhel <1840387+karmab@users.noreply.github.com>
2022-03-01 22:52:38 +0100

    openshift: simplify metal3 deployment (#388)

--- kvirt/openshift/disconnected/scripts/deploy.sh
+++ kvirt/openshift/disconnected/scripts/deploy.sh
@@ -1,7 +1,4 @@
 bash /root/scripts/01_get_oc.sh
-{% if metal3 %}
-bash /root/scripts/02_cache.sh
-{% endif %}
 bash /root/scripts/03_registry.sh
 bash /root/scripts/03_mirror.sh
 bash /root/scripts/04_extras.sh
