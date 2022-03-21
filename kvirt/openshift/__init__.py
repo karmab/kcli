534bee15dfee0b8ccc3184c8d530af8d2bbe6c17
karmab <karimboumedhel@gmail.com>
2022-03-21 19:55:48 +0100

    openshift sno: force extra_args to ip=dhcp6 with ipv6 and no dualstack

--- kvirt/openshift/__init__.py
+++ kvirt/openshift/__init__.py
@@ -524,6 +524,9 @@ def create(config, plandir, cluster, overrides, dnsconfig=None):
             warning("Forcing disconnected_deploy to True as no disconnected_url was provided")
             data['disconnected_deploy'] = True
             disconnected_deploy = True
+        if sno and not data['dualstack'] and 'extra_args' not in overrides:
+            warning("Forcing extra_args to ip=dhcp6 for sno to boot with ipv6")
+            data['extra_args'] = 'ip=dhcp6'
     ingress_ip = data.get('ingress_ip')
     if ingress_ip is not None and api_ip is not None and ingress_ip == api_ip:
         ingress_ip = None
