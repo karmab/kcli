163cfb70c850c5cf59a7c9fd20701c44cd299e94
karmab <karimboumedhel@gmail.com>
2022-04-10 14:58:09 +0200

    openshift disconnected: retry podman create to work around quay issues

--- kvirt/openshift/disconnected/scripts/03_registry.sh
+++ kvirt/openshift/disconnected/scripts/03_registry.sh
@@ -35,4 +35,5 @@ cp /opt/registry/certs/domain.crt /etc/pki/ca-trust/source/anchors/
 update-ca-trust extract
 htpasswd -bBc /opt/registry/auth/htpasswd $REGISTRY_USER $REGISTRY_PASSWORD
 podman create --name registry --net host --security-opt label=disable -v /opt/registry/data:/var/lib/registry:z -v /opt/registry/auth:/auth:z -v /opt/registry/conf/config.yml:/etc/docker/registry/config.yml -e "REGISTRY_AUTH=htpasswd" -e "REGISTRY_AUTH_HTPASSWD_REALM=Registry" -e "REGISTRY_HTTP_SECRET=ALongRandomSecretForRegistry" -e REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd -v /opt/registry/certs:/certs:z -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/domain.crt -e REGISTRY_HTTP_TLS_KEY=/certs/domain.key quay.io/saledort/registry:2
+[ "$?" == "0" ] || !!
 systemctl enable --now registry
