5af538fe0d53a60d93ac964d59406cc75d34fed1
karmab <karimboumedhel@gmail.com>
2022-05-02 10:24:34 +0200

    k3s: cleanup code

--- kvirt/k3s/__init__.py
+++ kvirt/k3s/__init__.py
@@ -14,18 +14,18 @@ def scale(config, plandir, cluster, overrides):
     data = {'cluster': cluster, 'kube': cluster, 'kubetype': 'k3s', 'image': 'ubuntu2004', 'sdn': 'flannel'}
     data['basedir'] = '/workdir' if container_mode() else '.'
     cluster = data.get('cluster')
-    clusterdir = os.path.expanduser("~/.kcli/clusters/%s" % cluster)
-    if os.path.exists("%s/kcli_parameters.yml" % clusterdir):
-        with open("%s/kcli_parameters.yml" % clusterdir, 'r') as install:
+    clusterdir = os.path.expanduser(f"~/.kcli/clusters/{cluster}")
+    if os.path.exists(f"{clusterdir}/kcli_parameters.yml"):
+        with open(f"{clusterdir}/kcli_parameters.yml", 'r') as install:
             installparam = yaml.safe_load(install)
             data.update(installparam)
             plan = installparam.get('plan', plan)
     data.update(overrides)
     sdn = data['sdn']
-    with open("%s/kcli_parameters.yml" % clusterdir, 'w') as paramfile:
+    with open(f"{clusterdir}/kcli_parameters.yml", 'w') as paramfile:
         yaml.safe_dump(data, paramfile)
     client = config.client
-    pprint("Scaling on client %s" % client)
+    pprint(f"Scaling on client {client}")
     os.chdir(os.path.expanduser("~/.kcli"))
     for role in ['masters', 'workers']:
         overrides = data.copy()
@@ -37,14 +37,14 @@ def scale(config, plandir, cluster, overrides):
             install_k3s_args = []
             for arg in data:
                 if arg.startswith('install_k3s'):
-                    install_k3s_args.append("%s=%s" % (arg.upper(), data[arg]))
+                    install_k3s_args.append(f"{arg.upper()}={data[arg]}")
             if sdn is None or sdn != 'flannel':
                 install_k3s_args.append("INSTALL_K3S_EXEC='--flannel-backend=none'")
             install_k3s_args = ' '.join(install_k3s_args)
             overrides['install_k3s_args'] = install_k3s_args
         if role == 'workers' and overrides.get('workers', 0) == 0:
             continue
-        config.plan(plan, inputfile='%s/%s.yml' % (plandir, role), overrides=overrides, threaded=threaded)
+        config.plan(plan, inputfile=f'{plandir}/{role}.yml', overrides=overrides, threaded=threaded)
 
 
 def create(config, plandir, cluster, overrides):
@@ -63,7 +63,7 @@ def create(config, plandir, cluster, overrides):
     if masters > 1:
         if platform in cloudplatforms:
             domain = data.get('domain', 'karmalabs.com')
-            api_ip = "%s-master.%s" % (cluster, domain)
+            api_ip = f"{cluster}-master.{domain}"
         elif api_ip is None:
             if network == 'default' and platform == 'kvm':
                 warning("Using 192.168.122.253 as api_ip")
@@ -71,36 +71,36 @@ def create(config, plandir, cluster, overrides):
                 api_ip = "192.168.122.253"
             elif platform == 'kubevirt':
                 selector = {'kcli/plan': plan, 'kcli/role': 'master'}
-                api_ip = config.k.create_service("%s-api" % cluster, config.k.namespace, selector,
+                api_ip = config.k.create_service(f"{cluster}-api", config.k.namespace, selector,
                                                  _type="LoadBalancer", ports=[6443])
                 if api_ip is None:
                     sys.exit(1)
                 else:
-                    pprint("Using api_ip %s" % api_ip)
+                    pprint(f"Using api_ip {api_ip}")
                     data['api_ip'] = api_ip
             else:
                 error("You need to define api_ip in your parameters file")
                 sys.exit(1)
         if data.get('virtual_router_id') is None:
             data['virtual_router_id'] = hash(data['cluster']) % 254 + 1
-            pprint("Using keepalived virtual_router_id %s" % data['virtual_router_id'])
+            pprint(f"Using keepalived virtual_router_id {data['virtual_router_id']}")
     data['basedir'] = '/workdir' if container_mode() else '.'
     install_k3s_args = []
     for arg in data:
         if arg.startswith('install_k3s'):
-            install_k3s_args.append("%s=%s" % (arg.upper(), data[arg]))
+            install_k3s_args.append(f"{arg.upper()}={data[arg]}")
     cluster = data.get('cluster')
-    clusterdir = os.path.expanduser("~/.kcli/clusters/%s" % cluster)
-    firstmaster = "%s-master-0" % cluster
+    clusterdir = os.path.expanduser(f"~/.kcli/clusters/{cluster}")
+    firstmaster = f"{cluster}-master-0"
     if os.path.exists(clusterdir):
-        error("Please remove existing directory %s first..." % clusterdir)
+        error(f"Please remove existing directory {clusterdir} first...")
         sys.exit(1)
     if find_executable('kubectl') is None:
         get_kubectl()
     if not os.path.exists(clusterdir):
         os.makedirs(clusterdir)
-        os.mkdir("%s/auth" % clusterdir)
-        with open("%s/kcli_parameters.yml" % clusterdir, 'w') as p:
+        os.mkdir(f"{clusterdir}/auth")
+        with open(f"{clusterdir}/kcli_parameters.yml", 'w') as p:
             installparam = overrides.copy()
             installparam['api_ip'] = api_ip
             installparam['plan'] = plan
@@ -116,7 +116,7 @@ def create(config, plandir, cluster, overrides):
         bootstrap_install_k3s_args.append("INSTALL_K3S_EXEC='--flannel-backend=none'")
     bootstrap_install_k3s_args = ' '.join(bootstrap_install_k3s_args)
     bootstrap_overrides['install_k3s_args'] = bootstrap_install_k3s_args
-    result = config.plan(plan, inputfile='%s/bootstrap.yml' % plandir, overrides=bootstrap_overrides)
+    result = config.plan(plan, inputfile=f'{plandir}/bootstrap.yml', overrides=bootstrap_overrides)
     if result['result'] != "success":
         sys.exit(1)
     nodes_overrides = data.copy()
@@ -128,15 +128,15 @@ def create(config, plandir, cluster, overrides):
     if masters > 1:
         pprint("Deploying extra masters")
         threaded = data.get('threaded', False) or data.get('masters_threaded', False)
-        config.plan(plan, inputfile='%s/masters.yml' % plandir, overrides=nodes_overrides, threaded=threaded)
-    with open("%s/join.sh" % clusterdir, 'w') as f:
+        config.plan(plan, inputfile=f'{plandir}/masters.yml', overrides=nodes_overrides, threaded=threaded)
+    with open(f"{clusterdir}/join.sh", 'w') as f:
         if api_ip is None:
             api_ip = k.info(firstmaster)['ip']
         joincmd = f"curl -sfL https://get.k3s.io | K3S_URL=https://{api_ip}:6443 K3S_TOKEN={token} "
         joincmd += ' '.join(install_k3s_args)
         extra_args = data['extra_worker_args'] if data.get('extra_worker_args', []) else data.get('extra_args', [])
         extra_args = ' '.join(extra_args)
-        f.write("%s sh -s - agent %s \n" % (joincmd, extra_args))
+        f.write(f"{joincmd} sh -s - agent {extra_args} \n")
     workers = data.get('workers', 0)
     if workers > 0:
         pprint("Deploying workers")
@@ -144,7 +144,7 @@ def create(config, plandir, cluster, overrides):
             del data['name']
         os.chdir(os.path.expanduser("~/.kcli"))
         threaded = data.get('threaded', False) or data.get('workers_threaded', False)
-        config.plan(plan, inputfile='%s/workers.yml' % plandir, overrides=data, threaded=threaded)
-    success("K3s cluster %s deployed!!!" % cluster)
-    info2("export KUBECONFIG=$HOME/.kcli/clusters/%s/auth/kubeconfig" % cluster)
+        config.plan(plan, inputfile=f'{plandir}/workers.yml', overrides=data, threaded=threaded)
+    success(f"K3s cluster {cluster} deployed!!!")
+    info2(f"export KUBECONFIG=$HOME/.kcli/clusters/{cluster}/auth/kubeconfig")
     info2("export PATH=$PWD:$PATH")
