fea4dd203cc14622e4a23c423dc900ba721ac1f9
karmab <karimboumedhel@gmail.com>
2022-05-03 13:26:44 +0200

    openshift app: support multus in ocs

--- kvirt/openshift/apps/ocs-operator/post.sh
+++ kvirt/openshift/apps/ocs-operator/post.sh
@@ -9,3 +9,10 @@ done
 echo Run the following command to make ocs-storagecluster-ceph-rbd the default storage class
 echo oc patch storageclass ocs-storagecluster-ceph-rbd -p \'{\"metadata\": {\"annotations\":{\"storageclass.kubernetes.io/is-default-class\":\"true\"}}}\'
 {% endif %}
+
+{% if ocs_public_network != None %}
+oc create -f nad_public.yml
+{% endif %}
+{% if ocs_cluster_network != None %}
+oc create -f nad_cluster.yml
+{% endif %}
