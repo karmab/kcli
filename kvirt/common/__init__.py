karmab/kcli

commit 6d7dadcbbd1f885089cf04141b5ebe27dc6495f8

dont put pub keys twice in cloudinit

kvirt/common/__init__.py
@@ -295,14 +295,20 @@ def cloudinit(name, keys=[], cmds=[], nets=[], gateway=None, dns=None, domain=No
             warning("no valid public keys found in .ssh/.kcli directories, you might have trouble accessing the vm")
         if keys:
             for key in list(set(keys)):
-                newkey = key
                 if os.path.exists(os.path.expanduser(key)):
                     keypath = os.path.expanduser(key)
                     newkey = open(keypath, 'r').read().rstrip()
+                else:
+                    newkey = key.rstrip()
                 if not newkey.startswith('ssh-'):
                     warning(f"Skipping invalid key {key}")
                     continue
                 userdata += "- %s\n" % newkey
+        if publickeyfile is not None:
+            with open(publickeyfile, 'r') as ssh:
+                key = ssh.read().rstrip()
+                if key not in keys:
+                    userdata += "- %s\n" % key
         tempkeydir = overrides.get('tempkeydir')
         if tempkeydir is not None:
             if not keys:
@@ -312,10 +318,6 @@ def cloudinit(name, keys=[], cmds=[], nets=[], gateway=None, dns=None, domain=No
             if not os.path.exists(privatekeyfile):
                 tempkeycmd = f"echo n | ssh-keygen -q -t rsa -N '' -C 'temp-kcli-key' -f {privatekeyfile}"
                 os.system(tempkeycmd)
-        if publickeyfile is not None:
-            with open(publickeyfile, 'r') as ssh:
-                key = ssh.read().rstrip()
-                userdata += "- %s\n" % key
         if cmds:
             data = process_cmds(cmds, overrides)
             if data != '':

commit 5d3a546c306e58cc61f40a950a9363d5ece48464

Fix the addition of pubkeys with no keys provided
In the last commit a bug was introduced when no `keys` were provided as
parameter. In that case, `keys` would have been `None` and the comparison
`if key not in keys` would fail:

~~~
  File "/usr/lib/python3.10/site-packages/kvirt/common/__init__.py", line 312, in cloudinit
    if key not in keys:
TypeError: argument of type 'NoneType' is not iterable
~~~

Fixes: 6d7dadcbbd1f ("dont put pub keys twice in cloudinit")

kvirt/common/__init__.py
@@ -307,7 +307,7 @@ def cloudinit(name, keys=[], cmds=[], nets=[], gateway=None, dns=None, domain=No
         if publickeyfile is not None:
             with open(publickeyfile, 'r') as ssh:
                 key = ssh.read().rstrip()
-                if key not in keys:
+                if keys is None or key not in keys:
                     userdata += "- %s\n" % key
         tempkeydir = overrides.get('tempkeydir')
         if tempkeydir is not None: