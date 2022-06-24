96a49c5239b051a5ffd4371e360a74398b6532b5
karmab <karimboumedhel@gmail.com>
2022-06-24 18:42:21 +0200

    openshift: update openstack.py helper sample

--- extras/openstack.py
+++ extras/openstack.py
@@ -2,10 +2,10 @@ from kvirt.config import Kconfig
 
 cluster = 'testk'
 network = "default"
-api_ip = "11.0.0.253"
-cidr = "11.0.0.0/24"
+api_ip = "12.0.0.253"
+cidr = "12.0.0.0/24"
 
 config = Kconfig()
-config.k.delete_network_port("%s-vip" % cluster)
+config.k.delete_network_port(f"{cluster}-vip" % cluster)
 config.k.create_network(name=network, cidr=cidr, overrides={'port_security_enabled': True})
-config.k.create_network_port("%s-vip" % cluster, network, ip=api_ip, floating=True)
+config.k.create_network_port(f"{cluster}-vip" % cluster, network, ip=api_ip, floating=True)
