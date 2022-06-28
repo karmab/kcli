71b0aad1d587c08958ffe4b031e48684ad1ae725
karmab <karimboumedhel@gmail.com>
2022-06-28 12:34:59 +0200

    openshift app: create knative-eventing in correct namespace

--- kvirt/openshift/apps/serverless-operator/post.sh
+++ kvirt/openshift/apps/serverless-operator/post.sh
@@ -2,4 +2,7 @@ echo """apiVersion: operator.knative.dev/v1alpha1
 kind: KnativeEventing
 metadata:
   name: knative-eventing
-  namespace: {{ namespace }}""" | oc apply -f -
+  namespace: knative-eventing""" | oc apply -f -
+{% if knative_eventing_inbroker %}
+oc label namespace knative-eventing eventing.knative.dev/injection=enabled
+{% endif %}
