8de673dbb7f5168d782bbe11381bca5515349db9
karmab <karimboumedhel@gmail.com>
2022-07-14 17:53:09 +0200

    hypershift: use correct secret for retrieving ignition

--- kvirt/hypershift/ignition.sh
+++ kvirt/hypershift/ignition.sh
@@ -2,8 +2,8 @@
 NAMESPACE={{ namespace }}-{{ cluster }}
 CLUSTER={{ cluster }}
 CLUSTERDIR={{ clusterdir }} 
-SECRET=$(oc -n $NAMESPACE get secret | grep token-$CLUSTER | awk '{print $1}')
-TOKEN=$(oc -n $NAMESPACE get secret $SECRET -o jsonpath={.data.token})
+SECRET=$(oc -n $NAMESPACE get secret | grep user-data-$CLUSTER | head -1 | awk '{print $1}')
+TOKEN=$(oc -n $NAMESPACE get secret $SECRET -o jsonpath='{.data.value}' | base64 -d )
 IP=$(oc get node -o wide | grep master | head -1 | awk '{print $6}')
 PORT=$(oc -n $NAMESPACE get svc ignition-server -o jsonpath={.spec.ports[0].nodePort})
-curl -k -H "Authorization: Bearer $TOKEN" https://$IP:$PORT/ignition > $CLUSTERDIR/worker.ign
+curl -k -H "Authorization: $TOKEN" https://$IP:$PORT/ignition > $CLUSTERDIR/worker.ign
