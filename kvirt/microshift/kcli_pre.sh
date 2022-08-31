0a39d7ed24d4b907ce813959d7a53a34a4f36982
karmab <karimboumedhel@gmail.com>
2022-08-31 14:44:04 +0200

    microshift: check open-cluster-management-image-pull-credentials secret is there when registering to acm

--- kvirt/microshift/kcli_pre.sh
+++ kvirt/microshift/kcli_pre.sh
@@ -1,3 +1,7 @@
-{% if register_acm and kubeconfig_acm == None %}
-echo register_acm requires to set kubeconfig_acm && exit 1
+{% if register_acm %}
+{% if kubeconfig_acm == None %}
+echo register_acm requires kubeconfig_acm to be set && exit 1
+{% else %}
+KUBECONFIG={{ kubeconfig_acm }} oc get secret -n open-cluster-management open-cluster-management-image-pull-credentials >/dev/null 2&1 || echo missing open-cluster-management-image-pull-credentials secret on acm cluster && exit 1
+{% endif %}
 {% endif %}
