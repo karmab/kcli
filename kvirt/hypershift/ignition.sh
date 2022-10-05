933fde0379b25164cca521adbf207fc4fe33ccb9
karmab <karimboumedhel@gmail.com>
2022-10-05 10:26:08 +0200

    hypershift: properly render base domain

--- kvirt/hypershift/ignition.sh
+++ kvirt/hypershift/ignition.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 NAMESPACE={{ namespace }}-{{ cluster }}
 CLUSTER={{ cluster }}
-DOMAIN={{ base_domain }}
+BASEDOMAIN={{ base_domain }}
 CLUSTERDIR={{ clusterdir }} 
 SECRET=$(oc -n $NAMESPACE get secret | grep user-data-$CLUSTER | head -1 | awk '{print $1}')
 TOKEN=$(oc -n $NAMESPACE get secret $SECRET -o jsonpath='{.data.value}' | base64 -d | awk -F "Bearer " '{print $2}' | awk -F "\"" '{print "Bearer " $1}')
