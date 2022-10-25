ada640a5bdfc16c479ce2bb778f3e6984b948301
karmab <karimboumedhel@gmail.com>
2022-10-25 12:12:07 +0200

    add alternative yes to please pipefail with tempkey

--- kvirt/common/__init__.py
+++ kvirt/common/__init__.py
@@ -292,8 +292,8 @@ def cloudinit(name, keys=[], cmds=[], nets=[], gateway=None, dns=None, domain=No
             privatekeyfile = f"{tempkeydir.name}/id_rsa"
             publickeyfile = f"{privatekeyfile}.pub"
             if not os.path.exists(privatekeyfile):
-                tempkeycmd = f"yes '' | ssh-keygen -q -t rsa -N '' -C 'temp-kcli-key' -f {privatekeyfile} > /dev/null"
-                tempkeycmd += " 2>&1"
+                tempkeycmd = f"yes '' | ssh-keygen -q -t rsa -N '' -C 'temp-kcli-key' -f {privatekeyfile} || yes"
+                tempkeycmd += " >/dev/null 2>&1 || yes"
                 os.system(tempkeycmd)
         userdata = '#cloud-config\n'
         userdata += 'final_message: kcli boot finished, up $UPTIME seconds\n'
