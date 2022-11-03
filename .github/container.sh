5806c7e03e9822d142c4a45ae8a8e5fa5f43c35f
karmab <karimboumedhel@gmail.com>
2022-11-03 20:59:54 +0100

    ci: dont login to docker.io

--- .github/container.sh
+++ .github/container.sh
@@ -5,7 +5,6 @@ set -ex
 TAG="$(git rev-parse --short HEAD)"
 GIT_VERSION="$TAG $(date +%Y/%m/%d)"
 echo $GIT_VERSION > kvirt/version/git
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD docker.io
 docker build -t quay.io/karmab/kcli:latest -f extras/debian .
 
 docker login -u $QUAY_USERNAME -p $QUAY_PASSWORD quay.io
--- .github/container_snapshot.sh
+++ .github/container_snapshot.sh
@@ -5,7 +5,6 @@ set -ex
 TAG="$(date +%y.%m)"
 GIT_VERSION="$TAG $(date +%Y/%m/%d)"
 echo $GIT_VERSION > kvirt/version/git
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD docker.io
 docker build -t quay.io/karmab/kcli:$TAG -f extras/debian .
 
 docker login -u $QUAY_USERNAME -p $QUAY_PASSWORD quay.io
