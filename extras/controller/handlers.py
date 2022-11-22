8b625b5dcc0b974a45fb09d4fed4630243dc7faa
karmab <karimboumedhel@gmail.com>
2022-11-22 17:12:27 +0100

    controller: cleanup

--- extras/controller/handlers.py
+++ extras/controller/handlers.py
@@ -2,8 +2,7 @@
 
 import kopf
 from kvirt.config import Kconfig
-from kvirt import common
-import os
+from kvirt.common import pprint, error
 from re import sub
 
 DOMAIN = "kcli.karmalabs.local"
@@ -14,7 +13,7 @@ def process_vm(name, namespace, spec, operation='create', timeout=60):
     config = Kconfig(quiet=True)
     exists = config.k.exists(name)
     if operation == "delete" and exists:
-        print("Deleting vm %s" % name)
+        pprint(f"Deleting vm {name}")
         return config.k.delete(name)
     if operation == "create":
         if not exists:
@@ -24,11 +23,10 @@ def process_vm(name, namespace, spec, operation='create', timeout=60):
                     profile = spec['image']
                 else:
                     profile = name
-            print("Creating vm %s" % name)
+            pprint(f"Creating vm {name}")
             if profile is not None:
                 result = config.create_vm(name, profile, overrides=dict(spec))
                 if result['result'] != 'success':
-                    print(result)
                     return result
         info = config.k.info(name)
         image = info.get('image')
@@ -40,17 +38,17 @@ def process_vm(name, namespace, spec, operation='create', timeout=60):
 def process_plan(plan, spec, operation='create'):
     config = Kconfig(quiet=True)
     if operation == "delete":
-        print("Deleting plan %s" % plan)
-        return config.plan(plan, delete=True)
+        pprint(f"Deleting plan {plan}")
+        return config.delete_plan(plan)
     else:
-        print("Creating/Updating plan %s" % plan)
+        pprint(f"Creating/Updating plan {plan}")
         overrides = spec.get('parameters', {})
         workdir = spec.get('workdir', '/workdir')
         inputstring = spec.get('plan')
         if inputstring is None:
-            print("Plan %s not created because of missing plan spec" % plan)
+            error("Plan %s not created because of missing plan spec" % plan)
             return {'result': 'failure', 'reason': 'missing plan spec'}
-        elif os.path.exists("/i_am_a_container"):
+        else:
             inputstring = sub(r"origin:( *)", r"origin:\1%s/" % workdir, inputstring)
             return config.plan(plan, inputstring=inputstring, overrides=overrides)
 
@@ -65,39 +63,39 @@ def update(name, namespace, diff):
         value = entry[3]
         if arg == 'plan':
             plan = value
-            common.pprint("Updating plan of vm %s to %s..." % (name, plan))
+            pprint(f"Updating plan of vm {name} to {plan}...")
             k.update_metadata(name, 'plan', plan)
         if arg == 'memory':
             memory = value
-            common.pprint("Updating memory of vm %s to %s..." % (name, memory))
+            pprint(f"Updating memory of vm {name} to {memory}...")
             k.update_memory(name, memory)
         if arg == 'numcpus':
             numcpus = value
-            common.pprint("Updating numcpus of vm %s to %s..." % (name, numcpus))
+            pprint(f"Updating numcpus of vm {name} to {numcpus}...")
             k.update_cpus(name, numcpus)
         if arg == 'autostart':
             autostart = value
-            common.pprint("Setting autostart for vm %s to %s..." % (name, autostart))
+            pprint(f"Setting autostart for vm {name} to {autostart}...")
             k.update_start(name, start=autostart)
         if arg == 'information':
             information = value
-            common.pprint("Setting information for vm %s..." % name)
+            pprint(f"Setting information for vm {name}...")
             k.update_information(name, information)
         if arg == 'iso':
             iso = value
-            common.pprint("Switching iso for vm %s to %s..." % (name, iso))
+            pprint(f"Switching iso for vm {name} to {iso}...")
             k.update_iso(name, iso)
         if arg == 'flavor':
             flavor = value
-            common.pprint("Updating flavor of vm %s to %s..." % (name, flavor))
+            pprint(f"Updating flavor of vm {name} to {flavor}...")
             k.update_flavor(name, flavor)
         if arg == 'start':
             start = value
             if start:
-                common.pprint("Starting vm %s..." % name)
+                pprint(f"Starting vm {name}...")
                 k.start(name)
             else:
-                common.pprint("Stopping vm %s..." % name)
+                pprint(f"Stopping vm {name}...")
                 k.stop(name)
         info = config.k.info(name)
         return info
@@ -107,7 +105,7 @@ def update(name, namespace, diff):
 def create_vm(meta, spec, status, namespace, logger, **kwargs):
     operation = 'create'
     name = meta.get('name')
-    print("Handling %s on vm %s" % (operation, name))
+    pprint(f"Handling {operation} on vm {name}")
     return process_vm(name, namespace, spec, operation=operation)
 
 
@@ -115,7 +113,7 @@ def create_vm(meta, spec, status, namespace, logger, **kwargs):
 def delete_vm(meta, spec, namespace, logger, **kwargs):
     operation = 'delete'
     name = meta.get('name')
-    print("Handling %s on vm %s" % (operation, name))
+    pprint(f"Handling {operation} on vm {name}")
     keep = spec.get("keep", False)
     if not keep:
         process_vm(name, namespace, spec, operation=operation)
@@ -125,7 +123,7 @@ def delete_vm(meta, spec, namespace, logger, **kwargs):
 def update_vm(meta, spec, namespace, old, new, diff, **kwargs):
     operation = 'update'
     name = meta.get('name')
-    print("Handling %s on vm %s" % (operation, name))
+    pprint(f"Handling {operation} on vm {name}")
     return update(name, namespace, diff)
 
 
@@ -133,7 +131,7 @@ def update_vm(meta, spec, namespace, old, new, diff, **kwargs):
 def create_plan(meta, spec, status, namespace, logger, **kwargs):
     operation = 'create'
     name = meta.get('name')
-    print("Handling %s on plan %s" % (operation, name))
+    pprint(f"Handling {operation} on plan {name}")
     return process_plan(name, spec, operation=operation)
 
 
@@ -142,14 +140,13 @@ def delete_plan(meta, spec, namespace, logger, **kwargs):
     operation = 'delete'
     name = meta.get('name')
     if spec.get('plan') is not None:
-        print("Handling %s on plan %s" % (operation, name))
+        pprint(f"Handling {operation} on plan {name}")
         process_plan(name, spec, operation=operation)
 
 
 @kopf.on.update(DOMAIN, VERSION, 'plans')
-# def update_plan(meta, spec, namespace, old, new, diff, **kwargs):
 def update_plan(meta, spec, status, namespace, logger, **kwargs):
     operation = 'update'
     name = meta.get('name')
-    print("Handling %s on vm %s" % (operation, name))
+    pprint(f"Handling {operation} on vm {name}")
     return process_plan(name, spec, operation=operation)
