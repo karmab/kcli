9f9c0045ed371adefc54145119205446e40c82c8
karmab <karimboumedhel@gmail.com>
2022-11-29 20:20:16 +0100

    ci: make kcli-controller and snapshot images multiarch

--- .github/container.sh
+++ .github/container.sh
@@ -8,8 +8,9 @@ echo $GIT_VERSION > kvirt/version/git
 podman build --arch=amd64 -t quay.io/karmab/kcli:amd64 -f extras/debian .
 podman build --arch=arm64 -t quay.io/karmab/kcli:arm64 -f extras/debian .
 
-podman login -u $QUAY_USERNAME -p $QUAY_PASSWORD quay.io
+podman images
 podman manifest create quay.io/karmab/kcli:latest
 podman manifest add quay.io/karmab/kcli:latest docker://quay.io/karmab/kcli:amd64
 podman manifest add quay.io/karmab/kcli:latest docker://quay.io/karmab/kcli:arm64
+podman login -u $QUAY_USERNAME -p $QUAY_PASSWORD quay.io
 podman manifest push --all quay.io/karmab/kcli:latest docker://quay.io/karmab/kcli:latest
--- .github/container_controller.sh
+++ .github/container_controller.sh
@@ -4,7 +4,12 @@ set -ex
 
 cd extras/controller
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-podman build -t quay.io/karmab/kcli-controller:latest .
+podman build --arch=amd64 -t quay.io/karmab/kcli-controller:amd64 .
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/arm64/kubectl"
+podman build --arch=arm64 -t quay.io/karmab/kcli-controller:arm64 .
 
 podman login -u $QUAY_USERNAME -p $QUAY_PASSWORD quay.io
-podman push quay.io/karmab/kcli-controller:latest
+podman manifest create quay.io/karmab/kcli-controller:latest
+podman manifest add quay.io/karmab/kcli-controller:latest docker://quay.io/karmab/kcli-controller:amd64
+podman manifest add quay.io/karmab/kcli-controller:latest docker://quay.io/karmab/kcli-controller:arm64
+podman manifest push --all quay.io/karmab/kcli-controller:latest  docker://quay.io/karmab/kcli-controller:latest
