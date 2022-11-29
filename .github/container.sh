0a42be9c9f8a95d1722a7235fb93a471b4cbe31b
karmab <karimboumedhel@gmail.com>
2022-11-29 20:08:13 +0100

    ci: make kcli container multiarch

--- .github/container.sh
+++ .github/container.sh
@@ -5,7 +5,11 @@ set -ex
 TAG="$(git rev-parse --short HEAD)"
 GIT_VERSION="$TAG $(date +%Y/%m/%d)"
 echo $GIT_VERSION > kvirt/version/git
-podman build -t quay.io/karmab/kcli:latest -f extras/debian .
+podman build --arch=amd64 -t quay.io/karmab/kcli:amd64 -f extras/debian .
+podman build --arch=arm64 -t quay.io/karmab/kcli:arm64 -f extras/debian .
 
 podman login -u $QUAY_USERNAME -p $QUAY_PASSWORD quay.io
-podman push quay.io/karmab/kcli:latest
+podman manifest create quay.io/karmab/kcli:latest
+podman manifest add quay.io/karmab/kcli:latest docker://quay.io/karmab/kcli:amd64
+podman manifest add quay.io/karmab/kcli:latest docker://quay.io/karmab/kcli:arm64
+podman manifest push --all quay.io/karmab/kcli:latest docker://quay.io/karmab/kcli:latest
