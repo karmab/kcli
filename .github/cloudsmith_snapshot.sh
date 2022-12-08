cd94b77fe5c3319a7d0d4003be7813cf0e477857
karmab <karimboumedhel@gmail.com>
2022-12-08 13:07:52 +0100

    ci: fix debian unknown compression for member control.tar.zst

--- .github/cloudsmith_snapshot.sh
+++ .github/cloudsmith_snapshot.sh
@@ -18,4 +18,14 @@ GIT_VERSION="$(curl -s https://github.com/karmab/kcli/commits/master | grep 'htt
 echo $GIT_VERSION > kvirt/version/git
 python3 setup.py --command-packages=stdeb.command sdist_dsc --debian-version $MINOR --depends python3-dateutil,python3-prettytable,python3-flask,python3-libvirt,python3-requests,genisoimage,python3-distutils bdist_deb
 deb=$(realpath $(find . -name *.deb))
+
+# ugly fix for unknown compression for member 'control.tar.zst', giving up
+ar x $deb
+unzstd control.tar.zst
+unzstd data.tar.zst
+xz --threads=0 --verbose control.tar
+xz --threads=0 --verbose data.tar
+rm -f $deb
+ar cr $deb debian-binary control.tar.xz data.tar.xz
+
 cloudsmith push deb karmab/kcli/any-distro/any-version $deb
--- .github/prep.sh
+++ .github/prep.sh
@@ -1,5 +1,5 @@
 apt-get update
-apt-get -y install python3-pip python3-wheel python3-setuptools python3-all python3-distutils dh-python debhelper build-essential fakeroot libkrb5-dev
+apt-get -y install python3-pip python3-wheel python3-setuptools python3-all python3-distutils dh-python debhelper build-essential fakeroot libkrb5-dev binutils
 pip3 install misspellings cloudsmith-cli pycodestyle stdeb copr-cli
 pip3 install -U Jinja2
 apt-get -y install libvirt-daemon libvirt0 qemu-system-x86 qemu-utils qemu-kvm libvirt-daemon-system curl genisoimage python3-libvirt qemu-user-static podman
