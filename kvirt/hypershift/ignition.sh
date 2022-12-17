394cb2acf939d4285a21342b2519d96d24ee306b
karmab <karimboumedhel@gmail.com>
2022-12-17 10:56:22 +0100

    hypershift: use ip of the nodes to fetch ignition

--- kvirt/hypershift/ignition.sh
+++ kvirt/hypershift/ignition.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+
+IP=$(oc get node -o wide | grep master | head -1 | awk '{print $6}')
 NAMESPACE={{ namespace }}-{{ cluster }}
 CLUSTER={{ cluster }}
-BASEDOMAIN={{ base_domain }}
 CLUSTERDIR={{ clusterdir }} 
 SECRET=$(oc -n $NAMESPACE get secret | grep user-data-$CLUSTER | head -1 | awk '{print $1}')
 TOKEN=$(oc -n $NAMESPACE get secret $SECRET -o jsonpath='{.data.value}' | base64 -d | awk -F "Bearer " '{print $2}' | awk -F "\"" '{print "Bearer " $1}')
 PORT=$(oc -n $NAMESPACE get svc ignition-server -o jsonpath={.spec.ports[0].nodePort})
-curl -k -H "Authorization: $TOKEN" https://api.$BASEDOMAIN:$PORT/ignition > $CLUSTERDIR/worker.ign
+curl -k -H "Authorization: $TOKEN" https://$IP:$PORT/ignition > $CLUSTERDIR/worker.ign
