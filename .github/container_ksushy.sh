430df14bac229537a17451958fb29775f6d79d30
karmab <karimboumedhel@gmail.com>
2022-12-20 01:07:48 +0100

    ksushy: correct iso name in mountcd.sh

--- .github/container_ksushy.sh
+++ .github/container_ksushy.sh
@@ -6,6 +6,6 @@ cd extras/ksushy
 
 git clone https://github.com/openshift-metal3/fakefish
 mv fakefish/* .
-podman build -t quay.io/karmab/kcli-sushy:latest .
+podman build -t quay.io/karmab/ksushy:latest .
 podman login -u $QUAY_USERNAME -p $QUAY_PASSWORD quay.io
-podman push quay.io/karmab/kcli-sushy:latest
+podman push quay.io/karmab/ksushy:latest
