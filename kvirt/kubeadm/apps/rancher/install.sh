4386c8770ca59cd6dc857d964d759fa96dccbe90
karmab <karimboumedhel@gmail.com>
2023-01-09 20:52:24 +0100

    apps generic: ingress rancher

--- kvirt/kubeadm/apps/rancher/install.sh
+++ kvirt/kubeadm/apps/rancher/install.sh
@@ -5,7 +5,7 @@ RANCHER_PASSWORD="{{ rancher_password }}"
 RANCHER_OPTS={{ "--devel" if rancher_version == 'alpha' else "" }}
 NAMESPACE="cattle-system"
 export PATH=$PWD:$PATH
-which helm || kcli download helm
+which helm >/dev/null 2>&1 || kcli download helm
 helm repo add rancher-$RANCHER_VERSION https://releases.rancher.com/server-charts/$RANCHER_VERSION
 kubectl create namespace $NAMESPACE
-helm install rancher rancher-$RANCHER_VERSION/rancher --namespace $NAMESPACE --set hostname=$HOSTNAME --set bootstrapPassword=$RANCHER_PASSWORD $RANCHER_OPTS
+helm install rancher rancher-$RANCHER_VERSION/rancher --namespace $NAMESPACE --set hostname=$HOSTNAME --set bootstrapPassword=$RANCHER_PASSWORD --set ingress.tls.source=rancher --set ingress.extraAnnotations.'kubernetes\.io/ingress\.class'=nginx $RANCHER_OPTS
--- kvirt/kubeadm/apps/rancher/uninstall.sh
+++ kvirt/kubeadm/apps/rancher/uninstall.sh
@@ -1,5 +1,5 @@
 NAMESPACE="cattle-system"
 export PATH=$PWD:$PATH
-which helm || kcli download helm
+which helm >/dev/null 2>&1 || kcli download helm
 helm uninstall rancher -n $NAMESPACE
 kubectl delete namespace $NAMESPACE
