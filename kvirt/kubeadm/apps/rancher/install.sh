8dd286d2f37d7d9c78c6fa27b2cb5ea996f16d15
karmab <karimboumedhel@gmail.com>
2023-01-12 14:26:19 +0100

    kube app: properly evaluate hostname and allow to use an lb ip

--- kvirt/kubeadm/apps/rancher/install.sh
+++ kvirt/kubeadm/apps/rancher/install.sh
@@ -1,13 +1,33 @@
-{% if rancher_hostname == None %}
-echo rancher_hostname needs to be set && exit 1
+HOSTNAME=${{ rancher_hostname or ""}}
+{% if rancher_lb_ip != None %}
+kubectl create -f rancher_ip.yml
+kubectl create -f rancher_advertisements.yml
+IP=$(echo {{ rancher_lb_ip }} | sed 's/./-/g')
+HOSTNAME="${HOSTNAME:-rancher-$IP.sslip.io}"
+{% elif rancher_ingress %}
+IP=$(kubectl get svc -n ingress-nginx ingress-nginx-controller -o jsonpath='{.status.loadBalancer.ingress[0].ip}' | sed 's/./-/g')
+HOSTNAME="${HOSTNAME:-rancher-$IP.sslip.io}"
 {% endif %}
-HOSTNAME={{ rancher_hostname }}
+
+if [ "$HOSTNAME" == "" ] ; then
+echo Couldnt figure out which hostname to use. Set rancher_ingress or rancher_lb_ip
+exit 1
+else
+echo Using $HOSTNAME as hostname
+fi
 RANCHER_VERSION="{{ rancher_version }}"
 RANCHER_PASSWORD="{{ rancher_password }}"
-RANCHER_OPTS={{ "--devel" if rancher_version == 'alpha' else "" }}
+RANCHER_DEV_OPTS={{ "--devel" if rancher_version == 'alpha' else "" }}
+RANCHER_INGRESS_OPTS={{ "--set ingress.tls.source=rancher --set ingress.extraAnnotations.'kubernetes\.io/ingress\.class'=nginx" if rancher_ingress else "--set ingress.enabled false" }}
 NAMESPACE="cattle-system"
 export PATH=$PWD:$PATH
 which helm >/dev/null 2>&1 || kcli download helm
 helm repo add rancher-$RANCHER_VERSION https://releases.rancher.com/server-charts/$RANCHER_VERSION
 kubectl create namespace $NAMESPACE
-helm install rancher rancher-$RANCHER_VERSION/rancher --namespace $NAMESPACE --set hostname=$HOSTNAME --set bootstrapPassword=$RANCHER_PASSWORD --set ingress.tls.source=rancher --set ingress.extraAnnotations.'kubernetes\.io/ingress\.class'=nginx $RANCHER_OPTS
+helm install rancher rancher-$RANCHER_VERSION/rancher --namespace $NAMESPACE --set hostname=$HOSTNAME --set bootstrapPassword=$RANCHER_PASSWORD $RANCHER_INGRESS_OPTS $RANCHER_DEV_OPTS
+kubectl wait -n cattle-system $(kubectl get pod -n cattle-system -l app=rancher -o name) --for=condition=Ready
+
+{% if rancher_lb_ip != None %}
+kubectl annotate svc rancher -n $NAMESPACE metallb.universe.tf/address-pool='rancher-ip'
+kubectl patch svc rancher -n $NAMESPACE -p '{"spec":{"type":"LoadBalancer"}}'
+{% endif %}
