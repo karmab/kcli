6126a701505edf92291e1631ff2136d8b61f648c
karmab <karimboumedhel@gmail.com>
2023-01-21 10:15:32 +0100

    hypershift: store hc and nodepool in separate files

--- kvirt/config.py
+++ kvirt/config.py
@@ -1543,7 +1543,7 @@ class Kconfig(Kbaseconfig):
             if 'KUBECONFIG' not in os.environ:
                 warning("KUBECONFIG not set...Using .kube/config instead")
             call(f'oc delete -f {clusterdir}/autoapprovercron.yml', shell=True)
-            call(f'oc delete -f {clusterdir}/assets.yaml', shell=True)
+            call(f'oc delete -f {clusterdir}/hostedcluster.yaml', shell=True)
             if 'baremetal_iso' in clusterdata or 'baremetal_hosts' in clusterdata:
                 call('oc -n default delete all -l app=httpd-kcli', shell=True)
                 call('oc -n default delete svc httpd-kcli-svc', shell=True)
@@ -2876,7 +2876,7 @@ class Kconfig(Kbaseconfig):
             z.delete_dns(f"apps.{cluster}", domain)
         if hypershift:
             call(f'KUBECONFIG={clusterdir}/kubeconfig.base oc delete -f {clusterdir}/autoapprovercron.yml', shell=True)
-            call(f'KUBECONFIG={clusterdir}/kubeconfig.base oc delete -f {clusterdir}/assets.yaml', shell=True)
+            call(f'KUBECONFIG={clusterdir}/kubeconfig.base oc delete -f {clusterdir}/hostedcluster.yaml', shell=True)
             if 'baremetal_iso' in clusterdata or 'baremetal_hosts' in clusterdata:
                 call(f'KUBECONFIG={clusterdir}/kubeconfig.base oc -n default delete all -l app=httpd-kcli', shell=True)
                 call(f'KUBECONFIG={clusterdir}/kubeconfig.bas eoc -n default delete pvc httpd-kcli-pvc', shell=True)
