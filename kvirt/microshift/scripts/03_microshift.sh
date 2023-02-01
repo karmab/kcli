642cc08b4144242d717a1ff331b0b05385943cc4
karmab <karimboumedhel@gmail.com>
2023-02-01 12:30:15 +0100

    microshift rhel: support injecting custom manifests

--- kvirt/microshift/scripts/03_microshift.sh
+++ kvirt/microshift/scripts/03_microshift.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [ -d /root/manifests ] ; then
+ mkdir -p /etc/microshift/manifests
+ cp /root/manifests/*y*ml /etc/microshift/manifests
+fi
+
 dnf -y install microshift
 BASEDOMAIN={{ "$(hostname)" if sslip else cluster + '.' + domain }}
 sed -i "s@#baseDomain: .*@baseDomain: $BASEDOMAIN@" /etc/microshift/config.yaml.default
