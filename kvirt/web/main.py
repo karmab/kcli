5402b43c79fc3babbbc7affb88f4075aa19c6897
karmab <karimboumedhel@gmail.com>
2023-02-28 14:18:35 +0100

    kweb: allow to run in readonly mode

--- kvirt/web/main.py
+++ kvirt/web/main.py
@@ -1,8 +1,12 @@
 from kvirt.web import Kweb
+import sys
 
 
 def run():
-    web = Kweb()
+    readonly = len(sys.argv) == 2 and sys.argv[1] == '--readonly'
+    if readonly:
+        print("Enabling readonly mode")
+    web = Kweb(readonly=readonly)
     web.run()
 
 
