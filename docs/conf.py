4fd89cb4682248067ea6d8ec727a13dbb9205693
karmab <karimboumedhel@gmail.com>
2023-04-19 10:37:10 +0200

    doc: switch to pydata_sphinx_theme

--- docs/conf.py
+++ docs/conf.py
@@ -70,7 +70,7 @@ exclude_patterns = []
 
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'pydata_sphinx_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
