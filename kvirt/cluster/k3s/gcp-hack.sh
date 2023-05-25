e8ca621688aee2561215f5fb552d7766cb7be417
karmab <karimboumedhel@gmail.com>
2023-05-25 23:57:22 +0200

    k3s gcp: use api_ip properly as IP in gcp-hack when set

--- kvirt/cluster/k3s/gcp-hack.sh
+++ kvirt/cluster/k3s/gcp-hack.sh
@@ -3,7 +3,11 @@
 systemctl is-active --quiet k3s && exit 0
 
 while true ; do
-  LB_IP=$(getent hosts api.{{ cluster }}.{{ domain }} | awk '{ print $1 }')
+{% if api_ip == 'api.%s.%s' % (cluster, domain) %}
+  LB_IP=$(getent hosts {{ api_ip }} | awk '{ print $1 }')
+{% else %}
+  LB_IP={{ api_ip }}
+{% endif %}
   ip route del table local proto 66 local $LB_IP scope host dev ens4
   systemctl is-active --quiet k3s && ip route add table local proto 66 local $LB_IP scope host dev ens4 && exit 0
   sleep 10
