f03ef059bb88b9c167213bcbca8112b8cc381f57
larssb <larssb@users.noreply.github.com>
2023-06-13 21:41:40 +0200

    Use the clust-init server as the IP to --server
    This simplifies the communication path for extra servers when they need to join the initial control-plane node. The one that was bootstrapped with --cluster-init.
    It also removes the need for a control-plane fronting load balancer to be ready for the extra control-plane nodes to join.

--- kvirt/cluster/k3s/ctlplanes.sh
+++ kvirt/cluster/k3s/ctlplanes.sh
@@ -9,4 +9,7 @@ apt-get -y install curl
 echo bpffs /sys/fs/bpf bpf defaults 0 0 >> /etc/fstab
 mount /sys/fs/bpf
 {% endif %}
-curl -sfL https://get.k3s.io | {{ install_k3s_args|default("") }} K3S_TOKEN={{ token }} sh -s - server --server https://{{ api_ip }}:6443 {{ extra_args|join(" ") }}
+
+{% set firstctlplane = cluster + '-ctlplane-0' %}
+{% set join_ip = firstctlplane|kcli_info('ip', client) %}
+curl -sfL https://get.k3s.io | {{ install_k3s_args|default("") }} K3S_TOKEN={{ token }} sh -s - server --server https://{{ join_ip }}:6443 {{ extra_args|join(" ") }}
