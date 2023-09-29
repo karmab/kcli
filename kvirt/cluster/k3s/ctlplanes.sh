d2b12a7429e2a3da22622c27c4acd381364f929e
larssb <larssb@users.noreply.github.com>
2023-09-29 11:34:16 +0200

    [K3s] - Always use the `--tls-san` parameter on k3s server instances (#602)
    * tls-san to always
    
    Added to bootstrap.sh in this commit
    
    * --tls-san to all control-plane nodes
    
    This to ctlplanes.sh

--- kvirt/cluster/k3s/ctlplanes.sh
+++ kvirt/cluster/k3s/ctlplanes.sh
@@ -1,3 +1,5 @@
+
+
 {% if extra_ctlplane_args %}
 {% set extra_args = extra_ctlplane_args %}
 {% endif %}
@@ -9,4 +11,6 @@ apt-get -y install curl
 echo bpffs /sys/fs/bpf bpf defaults 0 0 >> /etc/fstab
 mount /sys/fs/bpf
 {% endif %}
-curl -sfL https://get.k3s.io | {{ install_k3s_args|default("") }} K3S_TOKEN={{ token }} sh -s - server --server https://{{ first_ip }}:6443 {{ extra_args|join(" ") }}
+
+export IP={{ api_ip if cloud_lb else "$(curl -s ifconfig.me)" if config_type in ['aws', 'gcp', 'ibmcloud'] else first_ip }}
+curl -sfL https://get.k3s.io | {{ install_k3s_args|default("") }} K3S_TOKEN={{ token }} sh -s - server --server https://{{ first_ip }}:6443 {{ extra_args|join(" ") }} --tls-san $IP
