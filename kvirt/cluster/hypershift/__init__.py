0c52b4eb37cdbb88a4ab6277275a4a3e20672795
karmab <karimboumedhel@gmail.com>
2023-11-03 11:48:14 +0100

    hypershift: allow to use latest available

--- kvirt/cluster/hypershift/__init__.py
+++ kvirt/cluster/hypershift/__init__.py
@@ -358,7 +358,9 @@ def create(config, plandir, cluster, overrides):
     supported_data = yaml.safe_load(os.popen("oc get cm/supported-versions -o yaml -n hypershift").read())['data']
     supported_versions = supported_versions = supported_data['supported-versions']
     versions = yaml.safe_load(supported_versions)['versions']
-    if str(tag) not in versions:
+    if version == 'latest':
+        tag = versions[-1]
+    elif str(tag) not in versions:
         msg = f"Invalid tag {tag}. Choose between {','.join(versions)}"
         return {'result': 'failure', 'reason': msg}
     management_cmd = "oc get ingresscontroller -n openshift-ingress-operator default -o jsonpath='{.status.domain}'"
