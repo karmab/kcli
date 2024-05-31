a3c030d044ae45582b429702fcdbcc9a87157e7c
karmab <karimboumedhel@gmail.com>
2024-05-31 20:54:35 +0200

    ci: create snapshot monthly

--- .github/container_snapshot.sh
+++ .github/container_snapshot.sh
@@ -2,9 +2,11 @@
 
 set -ex
 
-TAG="$(date +%y.%m)"
+[ -n "$COMMIT" ] && git checkout $COMMIT
+TAG="$(git rev-parse --short HEAD)"
 GIT_VERSION="$TAG $(date +%Y/%m/%d)"
 echo $GIT_VERSION > kvirt/version/git
+
 podman build -t quay.io/karmab/kcli:$TAG -f extras/debian .
 podman login -u $QUAY_USERNAME -p $QUAY_PASSWORD quay.io
 podman push quay.io/karmab/kcli:$TAG
