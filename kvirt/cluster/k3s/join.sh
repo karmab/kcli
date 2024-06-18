a64453f33e29907c9beb9a2e74e89f27b62ce915
Lukas Hundt Petersen <lhp@nemlig.com>
2024-06-18 14:39:23 +0200

    Add retry to k3s join.sh curl

--- kvirt/cluster/k3s/join.sh
+++ kvirt/cluster/k3s/join.sh
@@ -4,4 +4,4 @@
 {% set api_ip = '{0}-ctlplane-1'.format(cluster)|kcli_info('ip') if scale|default(False) and 'ctlplane-0' in name else first_ip %}
 {% endif %}
 
-curl -sfL https://get.k3s.io | K3S_URL=https://{{ api_ip }}:6443 K3S_TOKEN={{ token }} {{ install_k3s_args|default([])|join(' ') }} sh -s - agent {{ extra_args|join(' ') }}
+curl --retry 5 -sfL https://get.k3s.io | K3S_URL=https://{{ api_ip }}:6443 K3S_TOKEN={{ token }} {{ install_k3s_args|default([])|join(' ') }} sh -s - agent {{ extra_args|join(' ') }}
