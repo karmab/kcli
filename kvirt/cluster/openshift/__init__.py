karmab/kcli

commit 1cf856d0e92f6ce0be92f26a157f9d65fc2635a9

openshift: imrprove ci tag detection

kvirt/cluster/openshift/__init__.py
@@ -844,7 +844,7 @@ def create(config, plandir, cluster, overrides, dnsconfig=None):
     if '0-ec.' in str(tag):
         version = 'dev-preview'
         data['version'] = version
-    elif float(tag) > float(OPENSHIFT_TAG):
+    elif int(tag.split('.')[1]) > int(OPENSHIFT_TAG.split('.')[1]):
         version = 'ci'
         data['version'] = version
     if os.path.exists('coreos-installer'):

commit cfcdd1e45459d076b5e4bc6c83a60463baf02761

openshift: cast openshift version to string
`tag: 4.17` in the parameter file is assumed to be a float and
causes the following error :

```
File "/usr/lib/python3.12/site-packages/kvirt/cluster/openshift/__init__.py", line 847, in create
    elif int(tag.split('.')[1]) > int(OPENSHIFT_TAG.split('.')[1]):
             ^^^^^^^^^
AttributeError: 'float' object has no attribute 'split'
```

Use `str(tag)` as everywhere else in the `create()` function.

Fixes: 1cf856d0e92f ("openshift: imrprove ci tag detection")
Signed-off-by: Greg Kurz <groug@kaod.org>

kvirt/cluster/openshift/__init__.py
@@ -844,7 +844,7 @@ def create(config, plandir, cluster, overrides, dnsconfig=None):
     if '0-ec.' in str(tag):
         version = 'dev-preview'
         data['version'] = version
-    elif int(tag.split('.')[1]) > int(OPENSHIFT_TAG.split('.')[1]):
+    elif int(str(tag).split('.')[1]) > int(OPENSHIFT_TAG.split('.')[1]):
         version = 'ci'
         data['version'] = version
     if os.path.exists('coreos-installer'):