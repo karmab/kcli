4ce9268b6f9c052857d605738ff6e5076f0ddbbc
karmab <karimboumedhel@gmail.com>
2025-01-17 10:49:22 +0100

    kubeadm registry: add docker creds

--- kvirt/cluster/kubeadm/disconnected/scripts/01_creds.sh
+++ kvirt/cluster/kubeadm/disconnected/scripts/01_creds.sh
@@ -3,5 +3,12 @@ REGISTRY_NAME={{ disconnected_vm_name or "$(echo $IP | sed 's/\./-/g' | sed 's/:
 REGISTRY={{ disconnected_vm_name or "$(echo $IP | sed 's/\./-/g' | sed 's/:/-/g').sslip.io" }}
 REGISTRY_USER={{ disconnected_user or "dummy" }}
 REGISTRY_PASSWORD={{ disconnected_password or "dummy" }}
-KEY=$( echo -n $REGISTRY_USER:$REGISTRY_PASSWORD | base64)
+KEY=$(echo -n $REGISTRY_USER:$REGISTRY_PASSWORD | base64)
 echo {\"auths\": {\"$REGISTRY:5000\": {\"auth\": \"$KEY\", \"email\": \"jhendrix@karmalabs.corp\"}}} > /root/kubeadm_pull.json
+{% if docker_user != None and docker_password != None %}
+DOCKER_USER={{ docker_user }}
+DOCKER_PASSWORD={{ docker_password }}
+DOCKER_KEY=$(echo -n $DOCKER_USER:$DOCKER_PASSWORD | base64)
+jq ".auths += {\"docker.io:5000\": {\"auth\": \"$DOCKER_KEY\",\"email\": \"jhendrix@karmalabs.corp\"}}" < /root/kubeadm_pull.json > /root/temp.json
+mv /root/temp.json /root/kubeadm_pull.json
+{% endif %}
