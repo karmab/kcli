f29c431df819649712c3fec0490784a0bb8b6b68
Gorka Eguileor <geguileo@redhat.com>
2025-04-09 10:23:45 +0200

    OpenStack: Fix using non v3applicationcredential
    When we are using the OpenStack provider with password or token it fails
    due to missing variables, this patch fixes it.

--- kvirt/config.py
+++ kvirt/config.py
@@ -272,6 +272,9 @@ class Kconfig(Kbaseconfig):
                 glance_disk = options.get('glance_disk', False)
                 auth_token = options.get('token') or os.environ.get("OS_TOKEN") or 'password'
                 default_auth_type = 'token' if auth_token is not None else 'password'
+                application_credential_id = None
+                application_credential_secret = None
+
                 auth_type = options.get('auth_type') or os.environ.get("OS_AUTH_TYPE") or default_auth_type
                 if auth_type == 'password' and password is None:
                     error("Missing password in the configuration. Leaving")
