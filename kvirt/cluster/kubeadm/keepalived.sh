78e07b5e55fc7fbda4879d3921af23df142b86ca
karmab <karimboumedhel@gmail.com>
2025-11-21 13:08:45 +0100

    kubeadm: get keepalived nic from default route

--- kvirt/cluster/kubeadm/keepalived.sh
+++ kvirt/cluster/kubeadm/keepalived.sh
@@ -2,7 +2,7 @@ PKGMGR="{{ 'apt-get' if ubuntu else 'dnf' }}"
 $PKGMGR -y install keepalived
 NETMASK=$(ip -o -f inet addr show | awk '/scope global/ {print $4}' | head -1 | cut -d'/' -f2)
 sed -i "s/NETMASK/$NETMASK/" /root/keepalived.conf
-NIC=$(find /sys/class/net -type l -not -lname '*virtual*' -printf '%f\n' | head -1)
+NIC=$(awk '/default/ {for(i=1;i<=NF;i++) if($i=="dev") {print $(i+1); exit}}' <(ip route show default; ip -6 route show default))
 sed -i "s/NIC/$NIC/" /root/keepalived.conf
 cp /root/keepalived.conf /etc/keepalived
 systemctl enable --now keepalived
