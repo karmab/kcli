86cacd3641100a26728c6f754d9523eb5844cd49
karmab <karimboumedhel@gmail.com>
2026-02-04 07:48:54 +0100

    CI: use cdn url

--- .github/container.sh
+++ .github/container.sh
@@ -4,7 +4,7 @@ TAG="$(git rev-parse --short HEAD)"
 GIT_VERSION="$TAG $(date +%Y/%m/%d)"
 echo $GIT_VERSION > kvirt/version/git
 
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://cdn.dl.k8s.io/release/$(curl -L -s https://cdn.dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 
 podman login -u $QUAY_USERNAME -p $QUAY_PASSWORD quay.io
 podman build -t quay.io/karmab/kcli:latest -f extras/debian .
--- .github/container_controller.sh
+++ .github/container_controller.sh
@@ -2,7 +2,7 @@
 
 cd extras/controller
 
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://cdn.dl.k8s.io/release/$(curl -L -s https://cdn.dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 podman build -t quay.io/karmab/kcli-controller:latest .
 podman login -u $QUAY_USERNAME -p $QUAY_PASSWORD quay.io
 podman push quay.io/karmab/kcli-controller:latest
