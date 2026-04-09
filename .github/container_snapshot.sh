3936b3bf7b71d13648c7a395d94692afb51b1faa
karmab <karimboumedhel@gmail.com>
2026-04-09 10:53:39 +0200

    CI: clearer Dockerfile

--- .github/container_snapshot.sh
+++ .github/container_snapshot.sh
@@ -7,18 +7,14 @@ echo $GIT_VERSION > kvirt/version/git
 
 TAG="${TAG:-$SHORT_TAG}"
 if [  -n "${EGG}"  ] ; then
-  sed -i "s/\[all\]/\[${EGG}\]/" extras/debian
+  sed -i "s/\[all\]/\[${EGG}\]/" extras/Dockerfile
   TAG="${EGG}-${TAG}"
 fi
 
 if [ "$TAG" == "arm64" ] ; then
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/arm64/kubectl"
-sed -i 's/bookworm/bookworm-arm64/' extras/debian
-grep FROM extras/debian
-else
-  curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+  sed -i 's/bookworm/bookworm-arm64/' extras/Dockerfile
 fi
 
 podman login -u $QUAY_USERNAME -p $QUAY_PASSWORD quay.io
-podman build -t quay.io/karmab/kcli:$TAG -f extras/debian .
+podman build -t quay.io/karmab/kcli:$TAG -f extras/Dockerfile .
 podman push quay.io/karmab/kcli:$TAG
