6eecfe675bcda500ab806efea90ace9fc748c808
karmab <karimboumedhel@gmail.com>
2026-08-10 12:19:09 +0200

    openshift app: check if csv is already installed

--- kvirt/cluster/openshift/apps/approve.sh
+++ kvirt/cluster/openshift/apps/approve.sh
@@ -1,13 +1,16 @@
 timeout=0
 while [ "$timeout" -lt "360" ] ; do
-  INSTALL_PLAN=$(oc get installplan -A -o json | jq -r '.items[] | select(.spec.approved==false) | select(.spec.clusterServiceVersionNames[] == "{{ csv }}") | "\(.metadata.namespace)/\(.metadata.name)"' 2>/dev/null)
-  [ -n "$INSTALL_PLAN" ] && break
-  echo "Waiting for InstallPlan for {{ csv }} to appear"
+  # Check if CSV is already installed
+  oc get csv -A --no-headers 2>/dev/null | grep -q "^.*{{ csv }}.*Succeeded" && break
+  INSTALL_PLAN=$(oc get installplan -A -o json | jq -r '.items[] | select(.spec.approved==false) | select(.spec.clusterServiceVersionNames[] == "{{
+csv }}") | "\(.metadata.namespace)/\(.metadata.name)"' 2>/dev/null)
+  if [ -n "$INSTALL_PLAN" ] ; then
+    NAMESPACE=$(echo $INSTALL_PLAN | cut -d'/' -f1)
+    PLAN=$(echo $INSTALL_PLAN | cut -d'/' -f2)
+    oc patch installplan -n $NAMESPACE $PLAN --type merge -p '{"spec":{"approved":true}}'
+  else
+    echo "Waiting for InstallPlan for {{ csv }} to appear"
+  fi
   sleep 5
   timeout=$(($timeout + 5))
 done
-if [ -n "$INSTALL_PLAN" ] ; then
-  NAMESPACE=$(echo $INSTALL_PLAN | cut -d'/' -f1)
-  PLAN=$(echo $INSTALL_PLAN | cut -d'/' -f2)
-  oc patch installplan -n $NAMESPACE $PLAN --type merge -p '{"spec":{"approved":true}}'
-fi
